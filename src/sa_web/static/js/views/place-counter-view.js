2b4db2608660fd0b79929a5c5c67031efa1f34a6
Luke Swart <lukeswart@gmail.com>
2015-08-01 02:59:44 -0700

    Implement auto-sizing and rendering of the meter

--- src/sa_web/static/js/views/place-counter-view.js
+++ src/sa_web/static/js/views/place-counter-view.js
@@ -4,11 +4,29 @@ var Shareabouts = Shareabouts || {};
 
 (function(S, $, console){
   S.PlaceCounterView = Backbone.View.extend({
+    initialize: function() {
+      var self = this;
+
+      self.numberOfPlaces = this.collection.models.length;
+
+      // Bind data events
+      self.collection.on('reset', self.render, self);
+      self.collection.on('add', self.incrementPlaces, self);
+      self.collection.on('remove', self.decrementPlaces, self);
+    },
+    incrementPlaces: function() {
+      this.numberOfPlaces++;
+      this.render();
+    },
+    decrementPlaces: function() {
+      this.numberOfPlaces--;
+      this.render();
+    },
     render: function() {
       var data = {
         length: this.collection.models.length,
         meter_config: this.options.mapConfig,
-        value: 2145
+        value: this.numberOfPlaces
       };
       this.$el.html(Handlebars.templates['count-meter'](data));
       return this;
