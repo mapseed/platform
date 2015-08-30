a953894d5293b1a6aa0ad1186f9fe21968ae0b03
Luke Swart <lukeswart@gmail.com>
2015-08-29 20:59:15 -0700

    Add new helper 'rain_garden_title'

--- src/flavors/raingardens/static/js/raingardens-handlebars-helpers.js
+++ src/flavors/raingardens/static/js/raingardens-handlebars-helpers.js
@@ -3,56 +3,18 @@
 var Shareabouts = Shareabouts || {};
 
 (function(NS) {
-  
-  Handlebars.registerHelper('thousands_comma', function(garden_number) {
-    // source: https://chiragrdarji.wordpress.com/2007/05/28/thousand-separator-function-for-java-script/
-    var decimalDigits = 0;
-    var Value = garden_number;
-    // Separator Length. Here this is thousand separator
-    var separatorLength = 3;
-    var OriginalValue=Value;
-    var TempValue = '' + OriginalValue;
-    var NewValue = '';
 
-    // Store digits after decimal
-    var pStr;
-
-    // store digits before decimal
-    var dStr;
-
-    // Add decimal point if it is not there
-    if (TempValue.indexOf('.')==-1)
-    {
-      TempValue+='.'
+  Handlebars.registerHelper('rain_garden_title', function(gardenNumber, gardenName) {
+    var gardenTitle = '';
+    if (gardenName) {
+      gardenTitle = gardenName;
+      if (gardenNumber)
+        gardenTitle +=  " - Rain Garden " + gardenNumber;
+    } else {
+      if (gardenNumber)
+        gardenTitle = "Rain Garden " + gardenNumber;
     }
-
-    dStr=TempValue.substr(0,TempValue.indexOf('.'));
-
-    pStr=TempValue.substr(TempValue.indexOf('.'))
-
-    // Add '0' for remaining digits after decimal point
-    while (pStr.length-1< decimalDigits){pStr+='0'}
-
-    if(pStr =='.')
-      pStr =''; 
-    
-    if(dStr.length > separatorLength)
-    {
-      // Logic of separation
-      while( dStr.length > separatorLength)
-      {
-        NewValue = ',' + dStr.substr(dStr.length - separatorLength) + NewValue;
-        dStr = dStr.substr(0,dStr.length - separatorLength);
-      }
-      NewValue = dStr + NewValue;
-    }
-    else
-    {
-      NewValue = dStr;
-    }
-    // Add decimal part
-    NewValue = NewValue + pStr;
-    return NewValue;
+    return gardenTitle;
   });
 
 }(Shareabouts));
