8d0bf1da192a0c33a0a064735759d9102c856ab0
tlake <tanner.lake@gmail.com>
2016-01-23 11:41:24 -0800

    beginning to merge old legend view into leaflet sidebar view

--- src/sa_web/static/js/views/leaflet-sidebar-view.js
+++ src/sa_web/static/js/views/leaflet-sidebar-view.js
@@ -3,18 +3,40 @@ var Shareabouts = Shareabouts || {};
 
 (function(S, $, console){
   S.LeafletSidebarView = Backbone.View.extend({
-    events: {},
-
+    events: {
+      'change .map-legend-checkbox': 'toggleVisibility'
+    },
     initialize: function () {
-        var self = this;
+      var self = this;
+      this.render();
+      self.sidebar = L.control.sidebar('leaflet-sidebar', {
+        position: 'left'
+      });
+      self.sidebar.addTo(this.options.mapView.map);
     },
 
-    render: function () {
-        var self = this;
+    render: function() {
+      var self = this,
+          data = _.extend({
+            reports: this.options.reports,
+            layers: this.options.layers
+          }, S.stickyFieldValues);
 
-        this.$el.html(Handlebars.templates['leaflet-sidebar']());
+      this.$el.html(Handlebars.templates['leaflet-sidebar-content'](data));
 
-        return this;
+      return this;
     },
+
+    // Checkbox change handler, triggers event to the MapView
+    toggleVisibility: function(evt) {
+      var $cbox = $(evt.target),
+        id = $cbox.attr('data-layerid');
+
+      if ($cbox.is(':checked')) {
+        $(S).trigger('visibility', [id, true]);
+      } else {
+        $(S).trigger('visibility', [id, false]);
+      }
+    }
   });
 })(Shareabouts, jQuery, Shareabouts.Util.console);
