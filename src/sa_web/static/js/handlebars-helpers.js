0996d9ff40e984b102981402ab9b9a2a9cfa5ca1
goldpbear <tcroxson@gmail.com>
2016-02-21 19:50:53 -0800

    add separate customizable display prompts for place detail view

--- src/sa_web/static/js/handlebars-helpers.js
+++ src/sa_web/static/js/handlebars-helpers.js
@@ -179,17 +179,17 @@ var Shareabouts = Shareabouts || {};
     options = args.slice(-1)[0];
     exclusions = args.slice(0, args.length-1);
 
-    console.log(this.from_dynamic_form);
-
     // check if a report has been made with the dynamic form, or with the regular form, and iterate appropriately
     _.each(this.from_dynamic_form ? NS.Config.place.categories[this.location_type].fields : NS.Config.place.items, function(item, i) {
     //_.each(NS.Config.place.items, function(item, i) {
+    //
+      
       var newItem = {
         name: item.name,
-        label: item.prompt,
+        label: item.display_prompt,
         value: this[item.name]
       };
-
+      
       // if not an exclusion and not private data
       if (_.contains(exclusions, item.name) === false &&
           item.name.indexOf('private-') !== 0) {
