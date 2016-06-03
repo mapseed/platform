68c11c785042e4e9eae55789fc4ff72d76898cec
goldpbear <tcroxson@gmail.com>
2016-06-02 18:49:27 -0700

    add basic story navigation feature to place detail views, controlled by a new object in the config

--- src/sa_web/static/js/views/app-view.js
+++ src/sa_web/static/js/views/app-view.js
@@ -558,6 +558,8 @@ var Shareabouts = Shareabouts || {};
       return landmarkDetailView;
     },
     getPlaceDetailView: function(model) {
+      console.log(model);
+
       var placeDetailView;
       if (this.placeDetailViews[model.cid]) {
         placeDetailView = this.placeDetailViews[model.cid];
