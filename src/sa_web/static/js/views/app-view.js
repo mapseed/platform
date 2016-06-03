0f45f96de1db87bacc542c708c77da185144ff4c
goldpbear <tcroxson@gmail.com>
2016-06-02 18:59:55 -0700

    remove console.logs

--- src/sa_web/static/js/views/app-view.js
+++ src/sa_web/static/js/views/app-view.js
@@ -493,8 +493,6 @@ var Shareabouts = Shareabouts || {};
       return landmarkDetailView;
     },
     getPlaceDetailView: function(model) {
-      console.log(model);
-
       var placeDetailView;
       if (this.placeDetailViews[model.cid]) {
         placeDetailView = this.placeDetailViews[model.cid];
