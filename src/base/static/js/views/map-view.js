d26f32893d85db864a489372d850432fef47cd59
goldpbear <tcroxson@gmail.com>
2016-08-16 21:28:59 -0700

    use a custom event to trigger user deletion of model, to avoid conflicting with other on destroy listeners

--- src/base/static/js/views/map-view.js
+++ src/base/static/js/views/map-view.js
@@ -106,7 +106,9 @@ var Shareabouts = Shareabouts || {};
       });
     }, // end initialize
 
-    onDestroyModel: function() {
+    onUserDeleteModel: function() {
+      console.log("onUserDeleteModel");
+
       S.Util.log('APP', 'panel-state', 'closed');
       // remove map mask if the user closes the side panel
       $("#spotlight-place-mask").remove();
