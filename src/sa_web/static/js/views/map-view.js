be2196b8280040ac323d2780bf60716bc0290386
goldpbear <tcroxson@gmail.com>
2016-08-18 16:40:17 -0700

    fix an issue where ghost markers remain after deleting a place via the in-browser editor

--- src/sa_web/static/js/views/map-view.js
+++ src/sa_web/static/js/views/map-view.js
@@ -107,8 +107,6 @@ var Shareabouts = Shareabouts || {};
     }, // end initialize
 
     onUserDeleteModel: function() {
-      console.log("onUserDeleteModel");
-
       S.Util.log('APP', 'panel-state', 'closed');
       // remove map mask if the user closes the side panel
       $("#spotlight-place-mask").remove();
