374a16d67f5bc1a4e2bd8444bec60c1e8035f3be
goldpbear <tcroxson@gmail.com>
2016-10-06 10:17:45 -0700

    In editor mode, hide models instead of deleting them

--- src/base/static/js/views/map-view.js
+++ src/base/static/js/views/map-view.js
@@ -106,7 +106,7 @@ var Shareabouts = Shareabouts || {};
       });
     }, // end initialize
 
-    onUserDeleteModel: function() {
+    onUserHideModel: function(model) {
       S.Util.log('APP', 'panel-state', 'closed');
       // remove map mask if the user closes the side panel
       $("#spotlight-place-mask").remove();
