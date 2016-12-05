98f15699caa8e01b6e23e15338360b7d72c5a2a8
goldpbear <tcroxson@gmail.com>
2016-12-05 14:22:44 -0800

    Refactor place/landmark loading
    - support places with landmark-style urls
    - reduce code duplication in AppView
    - slight loading performance boost on direct url loads
    
    Addresses: #527, #549

--- src/base/static/js/routes.js
+++ src/base/static/js/routes.js
@@ -11,6 +11,7 @@ var Shareabouts = Shareabouts || {};
       ':dataset/:id': 'viewPlace',
       'new': 'newPlace',
       ':dataset/:id/response/:response_id': 'viewPlace',
+      ':id/response/:response_id': 'viewLandmark',
       ':dataset/:id/edit': 'editPlace',
       'list': 'showList',
       ':id': 'viewLandmark',
@@ -183,12 +184,21 @@ var Shareabouts = Shareabouts || {};
       this.appView.newPlace();
     },
 
-    viewLandmark: function(id) {
-      this.appView.viewLandmark(id, { zoom: this.loading });
+    viewLandmark: function(modelId, responseId) {
+      this.appView.viewPlaceOrLandmark({
+        modelId: modelId,
+        responseId: responseId,
+        loading: this.loading
+      });
     },
 
-    viewPlace: function(datasetSlug, id, responseId) {
-      this.appView.viewPlace(datasetSlug, id, responseId, this.loading);
+    viewPlace: function(datasetSlug, modelId, responseId) {      
+      this.appView.viewPlaceOrLandmark({
+        datasetSlug: datasetSlug,
+        modelId: modelId,
+        responseId: responseId,
+        loading: this.loading
+      });
     },
 
     editPlace: function(){},
