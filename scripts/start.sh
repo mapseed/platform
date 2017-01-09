8fd3c266b2724b8aa62412c4e346ea6d29e27090
Luke Swart <luke@lukeswart.net>
2017-01-09 00:44:26 -0800

    temp hotfix for builds

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,7 @@
 
 # npm run build
 # python src/manage.py collectstatic --noinput
+cd ..
 python src/manage.py compilemessages
 gunicorn wsgi:application -w 3 -b 0.0.0.0:${PORT} --log-level=debug
+cd -
