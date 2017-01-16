cba6164151e3a5b5528a072f18203bbd6bb7eab4
Luke Swart <luke@lukeswart.net>
2017-01-15 20:24:46 -0800

    sa_web -> base folder rename

--- scripts/concat.js
+++ scripts/concat.js
@@ -10,72 +10,72 @@ try {
 var flavor = process.env.FLAVOR ? process.env.FLAVOR : "duwamish_flavor"
 
 var shell = require('shelljs')
-shell.rm('-rf', 'src/sa_web/static/dist')
-shell.mkdir('-p', 'src/sa_web/static/dist/images')
+shell.rm('-rf', 'src/base/static/dist')
+shell.mkdir('-p', 'src/base/static/dist/images')
 
 shell.cat([
-  'src/sa_web/static/libs/underscore.js',
-  'src/sa_web/static/libs/backbone.js',
-  'src/sa_web/static/libs/backbone.marionette.js',
-  'src/sa_web/static/libs/handlebars-v3.0.3.js',
-  'src/sa_web/static/libs/moment-with-locales.min.js',
-  'src/sa_web/static/libs/json2.js',
-  'src/sa_web/static/libs/binaryajax.js',
-  'src/sa_web/static/libs/exif.js',
-  'src/sa_web/static/libs/load-image.js',
-  'src/sa_web/static/libs/canvas-to-blob.js',
-  'src/sa_web/static/libs/spin.min.js',
-  'src/sa_web/static/libs/gatekeeper.js',
-  'src/sa_web/static/libs/swag.min.js',
-  'src/sa_web/static/libs/jquery.scrollTo.js',
-  'src/sa_web/static/libs/jquery.datetimepicker.full.min.js',
-  'src/sa_web/static/libs/handlebars-helpers.js',
-  'src/sa_web/static/libs/leaflet.argo.js',
-  'src/sa_web/static/libs/leaflet.sidebar.js',
-  'src/sa_web/static/libs/esri-leaflet.js'
-]).to('src/sa_web/static/dist/cat-lib-bundle.js')
+  'src/base/static/libs/underscore.js',
+  'src/base/static/libs/backbone.js',
+  'src/base/static/libs/backbone.marionette.js',
+  'src/base/static/libs/handlebars-v3.0.3.js',
+  'src/base/static/libs/moment-with-locales.min.js',
+  'src/base/static/libs/json2.js',
+  'src/base/static/libs/binaryajax.js',
+  'src/base/static/libs/exif.js',
+  'src/base/static/libs/load-image.js',
+  'src/base/static/libs/canvas-to-blob.js',
+  'src/base/static/libs/spin.min.js',
+  'src/base/static/libs/gatekeeper.js',
+  'src/base/static/libs/swag.min.js',
+  'src/base/static/libs/jquery.scrollTo.js',
+  'src/base/static/libs/jquery.datetimepicker.full.min.js',
+  'src/base/static/libs/handlebars-helpers.js',
+  'src/base/static/libs/leaflet.argo.js',
+  'src/base/static/libs/leaflet.sidebar.js',
+  'src/base/static/libs/esri-leaflet.js'
+]).to('src/base/static/dist/cat-lib-bundle.js')
 
 shell.cat([
-  'src/sa_web/static/js/handlebars-helpers.js',
-  'src/sa_web/static/js/models.js',
-  'src/sa_web/static/js/views/pages-nav-view.js',
-  'src/sa_web/static/js/views/auth-nav-view.js',
-  'src/sa_web/static/js/views/activity-view.js',
-  'src/sa_web/static/js/views/app-view.js',
-  'src/sa_web/static/js/views/layer-view.js',
-  'src/sa_web/static/js/views/basic-layer-view.js',
-  'src/sa_web/static/js/views/place-counter-view.js',
-  'src/sa_web/static/js/views/geocode-address-view.js',
-  'src/sa_web/static/js/views/geocode-address-place-view.js',
-  'src/sa_web/static/js/views/support-view.js',
-  'src/sa_web/static/js/views/survey-view.js',
-  'src/sa_web/static/js/views/landmark-survey-view.js',
-  'src/sa_web/static/js/views/place-detail-view.js',
-  'src/sa_web/static/js/views/landmark-detail-view.js',
-  'src/sa_web/static/js/views/place-form-view.js',
-  'src/sa_web/static/js/views/place-list-view.js',
-  'src/sa_web/static/js/views/map-view.js',
-  'src/sa_web/static/js/views/legend-view.js',
-  'src/sa_web/static/js/views/gis-legend-view.js',
-  'src/sa_web/static/js/views/sidebar-view.js',
-  'src/sa_web/static/js/routes.js',
+  'src/base/static/js/handlebars-helpers.js',
+  'src/base/static/js/models.js',
+  'src/base/static/js/views/pages-nav-view.js',
+  'src/base/static/js/views/auth-nav-view.js',
+  'src/base/static/js/views/activity-view.js',
+  'src/base/static/js/views/app-view.js',
+  'src/base/static/js/views/layer-view.js',
+  'src/base/static/js/views/basic-layer-view.js',
+  'src/base/static/js/views/place-counter-view.js',
+  'src/base/static/js/views/geocode-address-view.js',
+  'src/base/static/js/views/geocode-address-place-view.js',
+  'src/base/static/js/views/support-view.js',
+  'src/base/static/js/views/survey-view.js',
+  'src/base/static/js/views/landmark-survey-view.js',
+  'src/base/static/js/views/place-detail-view.js',
+  'src/base/static/js/views/landmark-detail-view.js',
+  'src/base/static/js/views/place-form-view.js',
+  'src/base/static/js/views/place-list-view.js',
+  'src/base/static/js/views/map-view.js',
+  'src/base/static/js/views/legend-view.js',
+  'src/base/static/js/views/gis-legend-view.js',
+  'src/base/static/js/views/sidebar-view.js',
+  'src/base/static/js/routes.js',
   'src/flavors/' + flavor + '/static/js/*.js',
   'src/flavors/' + flavor + '/static/js/views/*.js'
-]).to('src/sa_web/static/dist/cat-bundle.js')
+]).to('src/base/static/dist/cat-bundle.js')
 
 shell.cat([
-  'src/sa_web/static/js/utils.js',
-  'src/sa_web/static/js/template-helpers.js',
-]).to('src/sa_web/static/dist/cat-preload-bundle.js')
+  'src/base/static/js/utils.js',
+  'src/base/static/js/template-helpers.js',
+]).to('src/base/static/dist/cat-preload-bundle.js')
 
 shell.cat([
-  'src/sa_web/static/css/leaflet-sidebar.css',
-  'src/sa_web/static/css/default.css',
-  'src/sa_web/static/css/jquery.datetimepicker.css',
+  'src/base/static/css/leaflet-sidebar.css',
+  'src/base/static/css/default.css',
+  'src/base/static/css/jquery.datetimepicker.css',
   'src/flavors/' + flavor + '/static/css/custom.css'
-]).to('src/sa_web/static/dist/cat-bundle.css')
+]).to('src/base/static/dist/cat-bundle.css')
 
 shell.cp('-R', [
-  'src/sa_web/static/css/images/*',
+  'src/base/static/css/images/*',
   'src/flavors/' + flavor + '/static/css/images/*'
-], 'src/sa_web/static/dist/images')
+], 'src/base/static/dist/images')
