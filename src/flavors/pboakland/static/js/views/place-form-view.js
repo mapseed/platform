a605bfe40a6a3fe8ee5ae29a2211a4fcda2ed4f2
goldpbear <tcroxson@gmail.com>
2017-01-17 11:23:02 -0800

    Add pboakland admin-only form fields
    - All changes contained in pboakland flavor files

---
+++ src/flavors/pboakland/static/js/views/place-form-view.js
@@ -0,0 +1,314 @@
+/*globals _ Spinner Handlebars Backbone jQuery Gatekeeper */
+
+var Shareabouts = Shareabouts || {};
+
+(function(S, $, console){
+  S.PlaceFormView = Backbone.View.extend({
+    events: {
+      'submit form': 'onSubmit',
+      'change input[type="file"]': 'onInputFileChange',
+      'click .category-btn.clickable': 'onCategoryChange',
+      'click .category-menu-hamburger': 'onExpandCategories',
+      'click input[data-input-type="binary_toggle"]': 'onBinaryToggle',
+      'click .btn-geolocate': 'onClickGeolocate'
+    },
+    initialize: function(){
+      var self = this;
+       
+      this.resetFormState();
+      this.placeDetail = this.options.placeConfig.place_detail;
+
+      S.TemplateHelpers.overridePlaceTypeConfig(this.options.placeConfig.items,
+        this.options.defaultPlaceTypeName);
+      S.TemplateHelpers.insertInputTypeFlags(this.options.placeConfig.items);
+    },
+    resetFormState: function() {
+      this.formState = {
+        selectedCategoryConfig: {
+          fields: []
+        },
+        isSingleCategory: false,
+        attachmentData: null,
+        commonFormElements: this.options.placeConfig.common_form_elements || {}
+      }
+    },
+    render: function(isCategorySelected) {
+      var isAdmin = false,
+      self = this,
+      placesToIncludeOnForm = _.filter(this.placeDetail, function(place) { 
+        return place.includeOnForm; 
+      });
+
+      if (S.bootstrapped.currentUser &&
+        _.contains(this.options.placeConfig.administrators, S.bootstrapped.currentUser.username)) {
+        isAdmin = true;
+      }
+
+      // if there is only one place to include on form, skip category selection page
+      if (placesToIncludeOnForm.length === 1) {
+        this.formState.isSingleCategory = true;
+        isCategorySelected = true;
+        this.formState.selectedCategoryConfig = placesToIncludeOnForm[0];
+      }
+
+      this.checkAutocomplete();
+
+      var data = _.extend({
+        isCategorySelected: isCategorySelected,
+        isAdmin: isAdmin,
+        placeConfig: this.options.placeConfig,
+        selectedCategoryConfig: this.formState.selectedCategoryConfig,
+        user_token: this.options.userToken,
+        current_user: S.currentUser,
+        isSingleCategory: this.formState.isSingleCategory
+      }, S.stickyFieldValues);
+
+      this.$el.html(Handlebars.templates['place-form'](data));
+
+      if (this.center) $(".drag-marker-instructions").addClass("is-visuallyhidden");
+
+      $('#datetimepicker').datetimepicker({ formatTime: 'g:i a' });
+
+      return this;
+    },
+    // called from the app view
+    postRender: function() {
+      // NOTE: the extra call to initialize the date-time picker is necessary here,
+      // because on a single-category form the call to initialize in the render() method
+      // above will fail, since the form content will not yet have been inserted into
+      // the DOM by the app view
+      if (this.formState.isSingleCategory) {
+        $('#datetimepicker').datetimepicker({ formatTime: 'g:i a' });
+      }
+
+      this.bindCategoryListeners();
+    },
+    bindCategoryListeners: function() {
+      $(".category-btn-container").off().on("click", function(evt) {
+        $(this).prev().trigger("click");
+      });
+    },
+    checkAutocomplete: function() {
+      var self = this,
+      storedValue;
+
+      this.formState.selectedCategoryConfig.fields.forEach(function(field, i) {
+        storedValue = S.Util.getAutocompleteValue(field.name);
+        self.formState.selectedCategoryConfig.fields[i].autocompleteValue = storedValue || null;
+      });
+      this.formState.commonFormElements.forEach(function(field, i) {
+        storedValue = S.Util.getAutocompleteValue(field.name);
+        self.formState.commonFormElements[i].autocompleteValue = storedValue || null;
+      });
+    },
+    remove: function() {
+      this.unbind();
+    },
+    onError: function(model, res) {
+      // TODO handle model errors!
+      console.log('oh no errors!!', model, res);
+    },
+    // This is called from the app view
+    setLatLng: function(latLng) {
+      this.center = latLng;
+      this.$('.drag-marker-instructions, .drag-marker-warning').addClass('is-visuallyhidden');
+    },
+    setLocation: function(location) {
+      this.location = location;
+    },
+    getAttrs: function() {
+      var self = this,
+          attrs = {},
+          locationAttr = this.options.placeConfig.location_item_name,
+          $form = this.$('form');
+
+      // Get values from the form
+      attrs = S.Util.getAttrs($form);
+
+      // get values off of binary toggle buttons that have not been toggled
+      $.each($("input[data-input-type='binary_toggle']:not(:checked)"), function() {
+        attrs[$(this).attr("name")] = $(this).val();
+      });
+
+      _.each(attrs, function(value, key) {
+        var itemConfig = _.find(
+          self.formState.selectedCategoryConfig.fields
+            .concat(self.formState.commonFormElements), function(field) { 
+              return field.name === key;
+            }) || {};
+        if (itemConfig.autocomplete) {
+          S.Util.saveAutocompleteValue(key, value, 30);
+        }
+      });
+
+      // Get the location attributes from the map
+      attrs.geometry = {
+        type: 'Point',
+        coordinates: [this.center.lng, this.center.lat]
+      };
+
+      if (this.location && locationAttr) {
+        attrs[locationAttr] = this.location;
+      }
+
+      return attrs;
+    },
+    onCategoryChange: function(evt) {
+      var self = this,
+          animationDelay = 200;
+
+      this.formState.selectedCategoryConfig = _.find(this.placeDetail, function(place) {
+        return place.category == $(evt.target).attr('id');
+      });
+
+      this.render(true);
+      $("#" + $(evt.target).attr("id"))
+        .prop("checked", true)
+        .next()
+        .addClass("category-btn-container-selected");
+      $("#selected-category").hide().show(animationDelay);
+      $("#category-btns").animate( { height: "hide" }, animationDelay );
+      if (this.center) {
+        this.$('.drag-marker-instructions, .drag-marker-warning').addClass('is-visuallyhidden');
+      }
+    },
+    onExpandCategories: function(evt) {
+      var animationDelay = 200;
+      $("#selected-category").hide(animationDelay);
+      $("#category-btns").animate( { height: "show" }, animationDelay ); 
+      this.bindCategoryListeners();
+    },
+    onClickGeolocate: function(evt) {
+      var self = this;
+      evt.preventDefault();
+      var ll = this.options.appView.mapView.map.getBounds().toBBoxString();
+      S.Util.log('USER', 'map', 'geolocate', ll, this.options.appView.mapView.map.getZoom());
+      $("#drag-marker-content").addClass("is-visuallyhidden");
+      $("#geolocating-msg").removeClass("is-visuallyhidden");
+
+      this.options.appView.mapView.map.locate()
+        .on("locationfound", function() { 
+          self.center = self.options.appView.mapView.map.getCenter();
+          $("#spotlight-place-mask").remove();
+          $("#drag-marker-content").addClass("is-visuallyhidden");
+        })
+        .on("locationerror", function() {
+          $("#drag-marker-content").removeClass("is-visuallyhidden");
+          $("#geolocating-msg").addClass("is-visuallyhidden");
+        });
+    },
+    onInputFileChange: function(evt) {
+      var self = this,
+          file,
+          attachment;
+
+      if(evt.target.files && evt.target.files.length) {
+        file = evt.target.files[0];
+
+        this.$('.fileinput-name').text(file.name);
+        S.Util.fileToCanvas(file, function(canvas) {
+          canvas.toBlob(function(blob) {
+            self.formState.attachmentData = {
+              name: $(evt.target).attr('name'),
+              blob: blob,
+              file: canvas.toDataURL('image/jpeg')
+            }
+          }, 'image/jpeg');
+        }, {
+          // TODO: make configurable
+          maxWidth: 800,
+          maxHeight: 800,
+          canvas: true
+        });
+      }
+    },
+    onBinaryToggle: function(evt) {
+      var self = this,
+      targetButton = $(evt.target).attr("id"),
+      oldValue = $(evt.target).val(),
+      altData = _.find(this.formState.selectedCategoryConfig.fields
+        .concat(self.formState.commonFormElements), function(item) { 
+          return item.name === targetButton; 
+        }),
+      altContent = _.find(altData.content, function(item) { return item.value != oldValue; });
+
+      // set new value and label
+      $(evt.target).val(altContent.value);
+      $(evt.target).next("label").html(altContent.label);
+    },
+    closePanel: function() {
+      this.center = null;
+      this.resetFormState();
+    },
+    onSubmit: Gatekeeper.onValidSubmit(function(evt) {
+      // Make sure that the center point has been set after the form was
+      // rendered. If not, this is a good indication that the user neglected
+      // to move the map to set it in the correct location.
+      if (!this.center) {
+        this.$('.drag-marker-instructions').addClass('is-visuallyhidden');
+        this.$('.drag-marker-warning').removeClass('is-visuallyhidden');
+
+        // Scroll to the top of the panel if desktop
+        this.$el.parent('article').scrollTop(0);
+        // Scroll to the top of the window, if mobile
+        window.scrollTo(0, 0);
+        return;
+      }
+
+      var self = this,
+          router = this.options.router,
+          collection = this.collection[self.formState.selectedCategoryConfig.dataset],
+          model,
+          // Should not include any files
+          attrs = this.getAttrs(),
+          $button = this.$('[name="save-place-btn"]'),
+          spinner, $fileInputs;
+      evt.preventDefault();
+
+      collection.add({"location_type": this.formState.selectedCategoryConfig.category});
+      model = collection.at(collection.length - 1);
+
+      model.set("datasetSlug", _.find(this.options.mapConfig.layers, function(layer) { 
+        return self.formState.selectedCategoryConfig.dataset == layer.id;
+      }).slug);
+      model.set("datasetId", self.formState.selectedCategoryConfig.dataset);
+      
+      // if an attachment has been added...
+      if (self.formState.attachmentData) {
+        var attachment = model.attachmentCollection.find(function(attachmentModel) {
+          return attachmentModel.get('name') === self.formState.attachmentData.name;
+        });
+
+        if (_.isUndefined(attachment)) {
+          model.attachmentCollection.add(self.formState.attachmentData);
+        } else {
+          attachment.set(self.formState.attachmentData);
+        }
+      }
+
+      $button.attr('disabled', 'disabled');
+      spinner = new Spinner(S.smallSpinnerOptions).spin(self.$('.form-spinner')[0]);
+
+      S.Util.log('USER', 'new-place', 'submit-place-btn-click');
+
+      S.Util.setStickyFields(attrs, S.Config.survey.items, S.Config.place.items);
+
+      // Save and redirect
+      model.save(attrs, {
+        success: function() {
+          S.Util.log('USER', 'new-place', 'successfully-add-place');
+          router.navigate('/'+ model.get('datasetSlug') + '/' + model.id, {trigger: true});
+        },
+        error: function() {
+          S.Util.log('USER', 'new-place', 'fail-to-add-place');
+        },
+        complete: function() {
+          $button.removeAttr('disabled');
+          spinner.stop();
+          self.resetFormState();
+        },
+        wait: true
+      });
+    })
+  });
+}(Shareabouts, jQuery, Shareabouts.Util.console));
