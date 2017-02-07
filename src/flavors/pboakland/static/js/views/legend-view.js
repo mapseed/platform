cfc6b7d251eb191f1b3b42ca31051602dbe2080a
Zeb Burke-Conte <zmbc@uw.edu>
2017-02-07 14:25:20 -0800

    indent pboakland static views

--- src/flavors/pboakland/static/js/views/legend-view.js
+++ src/flavors/pboakland/static/js/views/legend-view.js
@@ -1,13 +1,13 @@
-module.exports = Backbone.View.extend({
-  render: function() {
-    var self = this,
-        data = _.extend({
-          items: this.options.config.items
-        }, Shareabouts.stickyFieldValues);
+  module.exports = Backbone.View.extend({
+    render: function() {
+      var self = this,
+          data = _.extend({
+            items: this.options.config.items
+          }, Shareabouts.stickyFieldValues);
 
-    // use prepend here so as to not overwrite activity stream
-    this.$el.prepend(Handlebars.templates['legend'](data));
+      // use prepend here so as to not overwrite activity stream
+      this.$el.prepend(Handlebars.templates['legend'](data));
 
-    return this;
-  }
-});
+      return this;
+    }
+  });
