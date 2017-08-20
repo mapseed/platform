757220be91241ed3300e0628d12bfd7b54b513f3
goldpbear <tcroxson@gmail.com>
2017-08-19 19:30:48 -0700

    (chore) fix jstemplates localization for static site build

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ require('dotenv').config({path: 'src/.env'});
 require("babel-polyfill");
 var path = require('path');
 var glob = require('glob');
+<<<<<<< 21425b24eb885b9f9ede8bda6f440371ba76219c
 var fs = require('fs');
 const shell = require('shelljs');
 
@@ -15,6 +16,16 @@ if (!process.env.FLAVOR) {
   process.exitCode = 1;
   process.exit();
 }
+=======
+var fs = require('fs-extra');
+var yaml = require('js-yaml');
+var Gettext = require("node-gettext");
+var gettextParser = require("gettext-parser");
+var walk = require("object-walk"); // object-walk supports traversal of JS objects
+var Handlebars = require("handlebars");
+var wax = require("wax-on"); // wax-on adds template inheritance to Handlebars
+var execSync = require("child_process").execSync;
+>>>>>>> (chore) fix jstemplates localization for static site build
 
 const distPath = path.resolve(
   __dirname,
