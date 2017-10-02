9bbefde463cedba6ca6f286a53f1bb98fbe572b4
goldpbear <tcroxson@gmail.com>
2017-10-02 16:37:19 -0700

    refactor(react port) add input form submit button

---
+++ src/base/static/components/input-form/input-form-submit-button.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import { SubmitField } from "../form-fields/submit-field";
+import { PrimaryButton } from "../ui-elements/primary-button";
+
+const baseClass = "input-form-submit-button";
+
+class InputFormSubmitButton extends Component {
+
+	render() {
+		return (
+			<div className={baseClass}>
+        <PrimaryButton>
+          {this.props.label}
+          <SubmitField name={this.props.name} />
+        </PrimaryButton>
+      </div>
+		);
+	}
+};
+
+export { InputFormSubmitButton };
