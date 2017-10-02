6476136c2d5c5c830c995b8f022efbe6c734fd5c
goldpbear <tcroxson@gmail.com>
2017-10-02 14:04:58 -0700

    refactor(react port) add input form attachment button

---
+++ src/base/static/components/form-fields/add-attachment-button.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import cx from "bem-classnames";
+
+import { SecondaryButton } from "../ui-elements/secondary-button";
+import { FileField } from "../form-fields/file-field";
+
+const Util = require("../../js/utils.js");
+
+const baseClass = "add-attachment-button";
+
+class AddAttachmentButton extends Component {
+
+  constructor() {
+    super(...arguments);
+    this.state = {
+      displayFilename: null
+    };
+    this.attachments = [];
+  }
+
+	onChange(evt) {
+		if (evt.target.files && evt.target.files.length) {
+			let file = evt.target.files[0];
+      this.setState({ displayFilename: file.name });
+
+      Util.fileToCanvas(
+        file,
+        (canvas) => {
+          canvas.toBlob((blob) => {
+            this.attachments.push({
+              name: this.props.name,
+              blob: blob,
+              file: canvas.toDataURL("image/jpeg"),
+            });
+          }, "image/jpeg");
+
+          console.log(this.attachments);
+        },
+        {
+          // TODO: make configurable
+          maxWidth: 800,
+          maxHeight: 800,
+          canvas: true,
+        },
+      );
+
+    }
+
+	}
+
+  render() {
+    let addAttachmentFilenameClass = {
+          name: baseClass + "__filename",
+          modifiers: ["visibility"]
+        };
+
+    return (
+      <div className={baseClass}>
+        <SecondaryButton>
+          Add an image
+          <FileField onChange={this.onChange.bind(this)} 
+                     name={this.props.name} />
+        </SecondaryButton>
+        <span className={cx(addAttachmentFilenameClass, { visibility: this.state.displayFilename ? "visible" : "hidden" })}>
+          {this.state.displayFilename}
+        </span>
+      </div>
+    );
+  }
+};
+
+export { AddAttachmentButton };
+
--- src/base/static/components/form-fields/file-field.js
+++ src/base/static/components/form-fields/file-field.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import cx from "bem-classnames";
 
 const baseClass = "mapseed-file-field";
 
 class FileField extends Component {
+
 	render() {
 		return (
 			<input 
@@ -11,7 +13,8 @@ class FileField extends Component {
 				id={this.props.id}
 				name={this.props.name}
 				value={this.props.value}
-				required={this.props.required} />
+				required={this.props.required} 
+				onChange={this.props.onChange.bind(this)} />
 		);
 	}
 };
