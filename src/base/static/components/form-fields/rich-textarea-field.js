5458506c1dfb7151d4f294281ee2d9d722915381
goldpbear <tcroxson@gmail.com>
2017-10-16 19:37:43 -0700

    refactor(react port) add fluid video dimension handling to rich text editor

--- src/base/static/components/form-fields/rich-textarea-field.js
+++ src/base/static/components/form-fields/rich-textarea-field.js
@@ -1,10 +1,47 @@
-import React, { Component, MouseEvent } from "react";
+import React, { Component } from "react";
 import cx from "bem-classnames";
-import ReactQuill from "react-quill";
+import ReactQuill, { Quill } from "react-quill";
+const BlockEmbed = Quill.import("blots/block/embed");
+const SnowTheme = Quill.import("themes/snow");
+const Link = Quill.import("formats/link");
 
 const Util = require("../../js/utils.js");
 
 const baseClass = "mapseed-rich-textarea-field";
+
+// NOTE: this routine is taken from Quill's themes/base module.
+const extractVideoUrl = (url) => {
+  let match = url.match(/^(?:(https?):\/\/)?(?:(?:www|m)\.)?youtube\.com\/watch.*v=([a-zA-Z0-9_-]+)/) ||
+              url.match(/^(?:(https?):\/\/)?(?:(?:www|m)\.)?youtu\.be\/([a-zA-Z0-9_-]+)/);
+  if (match) {
+    return (match[1] || 'https') + '://www.youtube.com/embed/' + match[2] + '?showinfo=0';
+  }
+  if (match = url.match(/^(?:(https?):\/\/)?(?:www\.)?vimeo\.com\/(\d+)/)) {
+    return (match[1] || 'https') + '://player.vimeo.com/video/' + match[2] + '/';
+  }
+  return url;
+};
+
+class WrappedVideo extends BlockEmbed {
+
+  static create(url) {
+    let node = super.create();
+    const iframe = document.createElement("iframe");
+    
+    url = Link.sanitize(extractVideoUrl(url));
+    iframe.setAttribute("src", url);
+    iframe.setAttribute("frameborder", 0);
+    iframe.setAttribute("allowfullscreen", true);
+    iframe.className = "ql-video";
+    node.appendChild(iframe);
+    
+    return node;
+  }
+};
+WrappedVideo.blotName = "wrappedVideo";
+WrappedVideo.tagName = "DIV";
+WrappedVideo.className = "ql-video-container";
+Quill.register(WrappedVideo);
 
 class RichTextareaField extends Component {
 
@@ -20,19 +57,56 @@ class RichTextareaField extends Component {
           ["link", "image", "video"],
         ],
         handlers: {
-          "image": this.onClickAddImage.bind(this)
+          "image": this.onClickEmbedImage.bind(this),
+          "video": this.onClickEmbedVideo.bind(this)
         }
       }
     };
   }
 
-  onClickAddImage() {
+  onClickEmbedImage() {
     this.refs["quill-file-input"].click();
+  }
+
+  onClickEmbedVideo(evt) {
+    this.snowTheme.tooltip.edit("video");
+  }
+
+  componentDidMount() {
+
+    let editor = this.refs["quill-editor"].getEditor();
+
+    // NOTE: we create a whole new SnowTheme here so we can make use of a
+    // tooltip box with custom click handler.
+    // TODO: is there a lighter-weight way to accomplish this?
+    this.snowTheme = new SnowTheme(editor, editor.options);
+    this.snowTheme.extendToolbar(editor.theme.modules.toolbar);
+
+    // We replace the ql-action element so we can attach our own click listener
+    // below.
+    let oldElt = this.snowTheme.tooltip.root.querySelector("a.ql-action"),
+        newElt = oldElt.cloneNode(true);
+    oldElt.parentNode.replaceChild(newElt, oldElt);
+
+    this.snowTheme.tooltip.root
+      .querySelector("a.ql-action")
+      .addEventListener("click", (evt) => {
+        evt.preventDefault();
+        editor.focus();
+        let url = this.snowTheme.tooltip.root.querySelector("input").value;
+        editor.insertEmbed(
+          editor.getSelection().index, 
+          "wrappedVideo", 
+          url, 
+          "user"
+        );
+        this.snowTheme.tooltip.root.className += " ql-hidden";
+      });
   }
 
   onAddImage(evt) {
 
-    // TODO: model/no model distinction here
+    // TODO: model/no model distinction here (i.e. form vs editor)
     if (evt.target.files && evt.target.files.length) {
       let file = evt.target.files[0];
       Util.fileToCanvas(
@@ -52,6 +126,9 @@ class RichTextareaField extends Component {
               data.file,
               "user",
             );
+
+            // Store attachment image data in the input form for processing later.
+            this.props.onAddImage(data);
 
             // if (self.options.placeDetailView) {
             //   // If we have a place detail view, we already have a model to which
@@ -95,11 +172,6 @@ class RichTextareaField extends Component {
     this.refs["quill-file-input"].value = "";
   }
 
-  onChange(evt) {
-    //console.log("rich textarea field onChange");
-
-  }
-
   render() {
     let quillFileInputClass = {
           name: baseClass + "__quill-file-input",
@@ -115,7 +187,7 @@ class RichTextareaField extends Component {
           placeholder={this.props.placeholder}
           value={this.props.value}
           bounds={this.props.bounds}
-          onChange={this.onChange.bind(this)}>
+          onChange={(value) => this.props.onChange(value, this.props.name)}>
         </ReactQuill>
         <input 
           className={cx(quillFileInputClass, { visibility: "hidden" })}
