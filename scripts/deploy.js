1627992358389d460e5f1847166458945a1b5b77
goldpbear <tcroxson@gmail.com>
2017-10-28 16:17:16 -0700

    chore(deploy.js) remove check for AWS keys

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,9 +2,6 @@ require('dotenv').config({path: 'src/.env'});
 if (!process.env.DEPLOY_DOMAIN) {
   throw 'Set the DEPLOY_DOMAIN environment variable to the domain you want to deploy Mapseed to.';
 }
-if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-  throw 'Set the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables to your AWS credentials.';
-}
 const config = {
   domain: process.env.DEPLOY_DOMAIN,
   region: process.env.DEPLOY_REGION || 'us-west-2',
