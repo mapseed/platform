f6c665cc47fa10d589dd553f8e90eb57f0aabe79
Zeb Burke-Conte <zmbc@uw.edu>
2017-11-03 23:58:19 -0700

    Return 200 on unknown pages, use Promises for asynchronous flow

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,56 +10,97 @@ const config = {
   region: process.env.DEPLOY_REGION || 'us-west-2',
   uploadDir: 'www',
   index: 'index.html',
-  error: 'index.html', // Setting as the error document allows client-side routing
   enableCloudfront: true
 };
 
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ region: config.region });
+const cloudfront = new AWS.CloudFront();
 
 const create = require('s3-website').s3site;
 const deploy = require('s3-website').deploy;
 
-const updateCacheControl = () => {
+function promisify(func, thisContext) {
+  if (thisContext) {
+    func = func.bind(thisContext);
+  }
+  return (...args) => {
+    return new Promise((resolve, reject) => {
+      func(...args, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    })
+  };
+}
+const createPromise = promisify(create);
+const deployPromise = promisify(deploy);
+const getDistributionConfigPromise = promisify(cloudfront.getDistributionConfig, cloudfront);
+const updateDistributionPromise = promisify(cloudfront.updateDistribution, cloudfront);
+const copyObjectPromise = promisify(s3.copyObject, s3);
 
+function updateCacheControl() {
+  console.log('Updating cache settings');
   // NOTE: We need to update the Cache-Control header for the index.html object,
   // as well as any localized index objects (such as es.html, for example),
   // so CloudFront won't cache these objects and they can fetch updated hashed
   // CSS and JS bundles consistently.
-  glob.sync("./www/*.html").forEach((path) => {
+  let updatePromises = glob.sync("./www/*.html").map((path) => {
     splitPath = path.split("/");
     let indexFile = splitPath[splitPath.length - 1];
     let params = {
-      Body: fs.readFileSync("./www/" + indexFile),
+      CopySource: encodeURIComponent(process.env.DEPLOY_DOMAIN + '/' + indexFile),
       Bucket: process.env.DEPLOY_DOMAIN,
       Key: indexFile,
       CacheControl: "no-cache",
-      ContentType: "text/html"
+      ContentType: "text/html",
+      MetadataDirective: 'REPLACE'
     };
-    s3.putObject(params, (err, data) => {
-      if (err) {
-        console.log(err, err.stack);
-      }
-    });
+    return copyObjectPromise(params);
   });
+
+  return Promise.all(updatePromises);
 }
 
-create(config, (err, website) => {
-  if (err && err.name === 'CNAMEAlreadyExists') {
-    // It's already been deployed
-    deploy(s3, config, (err, website) => {
-      if (err) {
-        throw err;
-      } else {
-        updateCacheControl();
-        console.log('Website deployed!');
-      }
+createPromise(config)
+  .then((website) => {
+    console.log('Getting cloudfront config');
+    let distConfig = getDistributionConfigPromise({Id: website.cloudfront.Distribution.Id});
+    return Promise.all([Promise.resolve(website), distConfig]);
+  })
+  .then(([website, response]) => {
+    console.log('Updating cloudfront config');
+    let config = response.DistributionConfig;
+    config.CustomErrorResponses = {
+      Quantity: 1,
+      Items: [
+        {
+          ErrorCode: 404,
+          ErrorCachingMinTTL: 300,
+          ResponseCode: '200',
+          ResponsePagePath: '/index.html'
+        }
+      ]
+    };
+    return updateDistributionPromise({
+      Id: website.cloudfront.Distribution.Id,
+      IfMatch: response.ETag,
+      DistributionConfig: config
     });
-    return;
-  } else if (err) {
-    throw err;
-  } else {
-    updateCacheControl();
-    console.log('Website created and deployed!');
-  }
-});
+  })
+  .then(() => deployPromise(s3, config))
+  .catch(err => {
+    if (err.name === 'CNAMEAlreadyExists') {
+      // It's already been deployed
+      console.log('Redeploying existing site');
+      return deployPromise(s3, config);
+    } else {
+      throw err;
+    }
+  })
+  .then(() => updateCacheControl())
+  .then(() => console.log('Website created and deployed!'))
+  .catch(e => console.log(e));
