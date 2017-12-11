5504c4f922a2f156e4d660fa791d2f76a47c071e
goldpbear <tcroxson@gmail.com>
2017-12-11 09:49:11 -0800

    (hotfix) refactor raingardens-handlebars-helpers.js for static site

--- src/flavors/raingardens/static/js/raingardens-handlebars-helpers.js
+++ src/flavors/raingardens/static/js/raingardens-handlebars-helpers.js
@@ -1,19 +1,13 @@
-/*global Handlebars _ moment */
-
-var Shareabouts = Shareabouts || {};
-
-(function(NS) {
-  Handlebars.registerHelper("rain_garden_title", function(
-    gardenNumber,
-    gardenName,
-  ) {
-    var gardenTitle = "";
-    if (gardenName) {
-      gardenTitle = gardenName;
-      if (gardenNumber) gardenTitle += " - Rain Garden " + gardenNumber;
-    } else {
-      if (gardenNumber) gardenTitle = "Rain Garden " + gardenNumber;
-    }
-    return gardenTitle;
-  });
-})(Shareabouts);
+Handlebars.registerHelper("rain_garden_title", function(
+  gardenNumber,
+  gardenName,
+) {
+  var gardenTitle = "";
+  if (gardenName) {
+    gardenTitle = gardenName;
+    if (gardenNumber) gardenTitle += " - Rain Garden " + gardenNumber;
+  } else {
+    if (gardenNumber) gardenTitle = "Rain Garden " + gardenNumber;
+  }
+  return gardenTitle;
+});
