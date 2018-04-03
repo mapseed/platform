d13c8694770dcd4c831f3fb3c85b0583fecac80b
modulitos <luke@lukeswart.net>
2018-04-03 00:56:40 -0700

    feat(InputFormTest) add snapshot test

--- src/base/static/components/input-form/__tests__/input-form.test.js
+++ src/base/static/components/input-form/__tests__/input-form.test.js
@@ -6,6 +6,28 @@ import constants from "../../../constants";
 
 describe("InputForm", () => {
   const eventMock = { preventDefault: () => {} };
+  test("renders input form", () => {
+    const props = {
+      container: {},
+      hideCenterPoint: () => {},
+      hideSpotlightMask: () => {},
+      selectedCategoryConfig: {
+        fields: [{ name: "test1" }, { name: "test2" }],
+      },
+      map: {
+        on: () => {},
+        off: () => {},
+        getCenter: () => {},
+      },
+      mapConfig: {},
+      places: {},
+      router: {},
+      showNewPin: () => {},
+    };
+
+    const wrapper = shallow(<InputForm {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
   test("renders form fields", () => {
     const props = {
       container: {},
