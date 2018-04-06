9da3e4fbbb9a2b018671ddf0b332d5d8d3fc9161
modulitos <luke@lukeswart.net>
2018-04-05 23:01:43 -0700

    feat(InputFormTest) add validation error and onSubmit testing

--- src/base/static/components/input-form/__tests__/input-form.test.js
+++ src/base/static/components/input-form/__tests__/input-form.test.js
@@ -9,56 +9,45 @@ import { OrderedMap, Map } from "immutable";
 describe("InputForm", () => {
   // TODO: consider generalizing this stub into a mock:
   const eventStub = { preventDefault: () => {} };
+  const defaultProps = {
+    container: {},
+    hideCenterPoint: () => {},
+    hideSpotlightMask: () => {},
+    selectedCategoryConfig: {
+      fields: [{ name: "test1" }, { name: "test2" }],
+    },
+    map: {
+      on: () => {},
+      off: () => {},
+      getCenter: () => {},
+    },
+    mapConfig: {},
+    places: {},
+    router: {},
+    showNewPin: () => {},
+  };
   test("renders input form", () => {
-    const props = {
-      container: {},
-      hideCenterPoint: () => {},
-      hideSpotlightMask: () => {},
-      selectedCategoryConfig: {
-        fields: [{ name: "test1" }, { name: "test2" }],
-      },
-      map: {
-        on: () => {},
-        off: () => {},
-        getCenter: () => {},
-      },
-      mapConfig: {},
-      places: {},
-      router: {},
-      showNewPin: () => {},
-    };
-
-    const wrapper = shallow(<InputForm {...props} />);
+    const wrapper = shallow(<InputForm {...defaultProps} />);
     expect(wrapper).toMatchSnapshot();
   });
-  test("renders form fields", () => {
-    const props = {
-      container: {},
-      hideCenterPoint: () => {},
-      hideSpotlightMask: () => {},
-      selectedCategoryConfig: {
-        fields: [{ name: "test1" }, { name: "test2" }],
-      },
-      map: {
-        on: () => {},
-        off: () => {},
-        getCenter: () => {},
-      },
-      mapConfig: {},
-      places: {},
-      router: {},
-      showNewPin: () => {},
-    };
 
-    const wrapper = shallow(<InputForm {...props} />);
+  test("renders form fields", () => {
+    const wrapper = shallow(<InputForm {...defaultProps} />);
     expect(wrapper.find(FormField)).toHaveLength(2);
   });
 
   test("renders form validation errors", () => {
+    const wrapper = shallow(<InputForm {...defaultProps} />);
+    wrapper.setState({
+      formValidationErrors: new Set(["asdf", "asdf2"]),
+    });
+
+    expect(wrapper.find(ErrorMessage)).toHaveLength(2);
+  });
+
+  test("onSubmit creates form validation errors", () => {
     const props = {
-      container: {},
-      hideCenterPoint: () => {},
-      hideSpotlightMask: () => {},
+      ...defaultProps,
       selectedCategoryConfig: {
         fields: [
           { name: "test1", [constants.FIELD_VALIDITY_KEY]: "error1" },
@@ -66,37 +55,31 @@ describe("InputForm", () => {
           { name: "test3", [constants.FIELD_VALIDITY_KEY]: "error3" },
         ],
       },
-      map: {
-        on: () => {},
-        off: () => {},
-        getCenter: () => {},
-      },
-      mapConfig: {},
-      places: {},
-      router: {},
-      showNewPin: () => {},
     };
 
     const wrapper = shallow(<InputForm {...props} />);
+    const errorKey1 = "error1";
+    const errorKey2 = "error2";
+    const errorKey3 = "error3";
     wrapper.setState({
       fields: OrderedMap({
         test1: Map({
           [constants.FIELD_RENDER_KEY]: 0,
           [constants.FIELD_VALUE_KEY]: "",
           [constants.FIELD_VALIDITY_KEY]: true,
-          [constants.FIELD_VALIDITY_MESSAGE_KEY]: "asdf1",
+          [constants.FIELD_VALIDITY_MESSAGE_KEY]: errorKey1,
         }),
         test2: Map({
           [constants.FIELD_RENDER_KEY]: 1,
           [constants.FIELD_VALUE_KEY]: "",
           [constants.FIELD_VALIDITY_KEY]: false,
-          [constants.FIELD_VALIDITY_MESSAGE_KEY]: "asdf2",
+          [constants.FIELD_VALIDITY_MESSAGE_KEY]: errorKey2,
         }),
         test3: Map({
           [constants.FIELD_RENDER_KEY]: 2,
           [constants.FIELD_VALUE_KEY]: "",
           [constants.FIELD_VALIDITY_KEY]: false,
-          [constants.FIELD_VALIDITY_MESSAGE_KEY]: "asdf3",
+          [constants.FIELD_VALIDITY_MESSAGE_KEY]: errorKey3,
         }),
       }),
       updatingField: null,
@@ -107,6 +90,8 @@ describe("InputForm", () => {
     });
 
     wrapper.instance().onSubmit(eventStub);
-    expect(wrapper.find(ErrorMessage)).toHaveLength(2);
+    expect(wrapper.state("formValidationErrors")).toEqual(
+      new Set([errorKey2, errorKey3]),
+    );
   });
 });
