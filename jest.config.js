bde5acc9aa89306172202f9bd18931c90affcb6a
modulitos <luke@lukeswart.net>
2018-04-08 15:40:34 -0700

    feat(package.json) add jest-runner-prettier

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  displayName: "mapseed-platform-test",
-  moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/file-mock.js",
-    "\\.(css|less|scss)$": "<rootDir>/__mocks__/style-mock.js",
-  },
-  setupFiles: ["<rootDir>/jest-setup.js"],
-  snapshotSerializers: ["enzyme-to-json/serializer"],
-  testMatch: ["**/*.test.js"],
+  projects: [
+    {
+      displayName: "jest-prettier",
+      runner: "jest-runner-prettier",
+      moduleFileExtensions: ["js"],
+      testMatch: ["<rootDir>/src/**/*.js", "<rootDir>/scripts/**/*.js"],
+    },
+    {
+      displayName: "mapseed-platform-test",
+      moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+          "<rootDir>/__mocks__/file-mock.js",
+        "\\.(css|less|scss)$": "<rootDir>/__mocks__/style-mock.js",
+      },
+      setupFiles: ["<rootDir>/jest-setup.js"],
+      snapshotSerializers: ["enzyme-to-json/serializer"],
+      testMatch: ["**/*.test.js"],
+    },
+  ],
 };
