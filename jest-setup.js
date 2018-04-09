7c551469dac1a105983989f4f1c718e91fa12ac7
Luke Swart <luke@lukeswart.net>
2018-04-09 14:02:39 -0700

    Luke/input form test refactor (#906)
    * refactor(inputFormTest) refactor react-i18next mock into jest setup file
    
    * refactor(InputForm) update WarningMessagesContainer render logic and snapshots

--- jest-setup.js
+++ jest-setup.js
@@ -2,4 +2,13 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import "raf/polyfill";
 
+// https://github.com/i18next/react-i18next/issues/417
+jest.mock("react-i18next", () => ({
+  // this mock makes sure any components using the translate HoC receive the t function as a prop
+  translate: () => Component => {
+    Component.defaultProps = { ...Component.defaultProps, t: () => "" };
+    return Component;
+  },
+}));
+
 Enzyme.configure({ adapter: new Adapter() });
