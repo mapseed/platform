2b5855ab830e8ca2e92008a9e63a9ea10f1d7366
goldpbear <tcroxson@gmail.com>
2018-04-09 18:08:06 -0700

    Revert "chore: remove unused moduleNameMapper entry"
    This reverts commit 4af74a178f5f18997eb23dcf3e33e6a561f1d1a9.

--- jest.config.js
+++ jest.config.js
@@ -19,6 +19,7 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
           "<rootDir>/__mocks__/file-mock.js",
         "\\.(css|less|scss)$": "<rootDir>/__mocks__/style-mock.js",
+        "^config$": "<rootDir>/__mocks__/config-mock.js",
       },
       setupFiles: ["<rootDir>/jest-setup.js"],
       snapshotSerializers: ["enzyme-to-json/serializer"],
