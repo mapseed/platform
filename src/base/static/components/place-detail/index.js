78c31816965afd75dc06fad35fbbf13dc53e6368
goldpbear <tcroxson@gmail.com>
2018-04-10 13:02:20 -0700

    (hotfix) fix attachment refresh on save in editor

--- src/base/static/components/place-detail/index.js
+++ src/base/static/components/place-detail/index.js
@@ -164,6 +164,7 @@ class PlaceDetail extends Component {
         isEditFormSubmitting: false,
         placeModel: fromJS(this.props.model.attributes),
       });
+      this.refreshAttachments();
     } else if (action === constants.PLACE_MODEL_IO_END_ERROR_ACTION) {
       this.setState({ isEditFormSubmitting: false });
     } else if (action === constants.SURVEY_MODEL_IO_END_SUCCESS_ACTION) {
@@ -221,7 +222,6 @@ class PlaceDetail extends Component {
             isEditModeToggled={this.state.isEditModeToggled}
             isSubmitting={this.state.isEditFormSubmitting}
             onToggleEditMode={() => {
-              this.refreshAttachments();
               this.setState({
                 isEditModeToggled: !this.state.isEditModeToggled,
               });
