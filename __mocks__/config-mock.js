197a28d1a5a719f18afff10b7f8be8419fa78840
goldpbear <tcroxson@gmail.com>
2018-04-09 17:45:00 -0700

    chore(input form test): update tests and snapshot

---
+++ __mocks__/config-mock.js
@@ -0,0 +1,26 @@
+module.exports = {
+  app: {},
+  place_types: {},
+  place: {
+    place_detail: [
+      {
+        category: "someCategory",
+        fields: [
+          {
+            name: "test1",
+          },
+          {
+            name: "test2",
+          },
+        ],
+      },
+    ],
+  },
+  survey: {},
+  support: {},
+  pages: {},
+  story: {},
+  sidebar: {},
+  activity: {},
+  cluster: {},
+};
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   projects: [
     {
       displayName: "jest-prettier",
+      moduleNameMapper: {
+        config: "<rootDir>/src/__mocks__/config-mock.js",
+      },
       runner: "jest-runner-prettier",
       moduleFileExtensions: ["js"],
       testMatch: ["<rootDir>/src/**/*.js", "<rootDir>/scripts/**/*.js"],
@@ -16,6 +19,7 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
           "<rootDir>/__mocks__/file-mock.js",
         "\\.(css|less|scss)$": "<rootDir>/__mocks__/style-mock.js",
+        "^config$": "<rootDir>/__mocks__/config-mock.js",
       },
       setupFiles: ["<rootDir>/jest-setup.js"],
       snapshotSerializers: ["enzyme-to-json/serializer"],
