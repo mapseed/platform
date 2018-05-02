8a09d603806c29be4c1a84e88481f2766092ec73
goldpbear <tcroxson@gmail.com>
2018-05-02 13:19:12 -0700

    chore: update tests

--- src/base/static/components/input-form/__tests__/input-form.test.js
+++ src/base/static/components/input-form/__tests__/input-form.test.js
@@ -49,50 +49,72 @@ describe("InputForm", () => {
     const fieldName3 = "test3";
     const props = {
       ...defaultProps,
-      selectedCategoryConfig: {
-        fields: [
-          { name: fieldName1, [constants.FIELD_VALIDITY_KEY]: "error1" },
-          { name: fieldName2 },
-          { name: fieldName3, [constants.FIELD_VALIDITY_KEY]: "error3" },
-        ],
-      },
     };
 
     const wrapper = shallow(<InputForm {...props} />);
-    const errorKey1 = "error1";
-    const errorKey2 = "error2";
-    const errorKey3 = "error3";
+    const errorMessage1 = "error1";
+    const errorMessage2 = "error2";
+    const errorMessage3 = "error3";
     wrapper.setState({
       fields: OrderedMap({
         [fieldName1]: Map({
-          [constants.FIELD_RENDER_KEY]: categoryName + fieldName1,
-          [constants.FIELD_VALUE_KEY]: "",
           [constants.FIELD_VALIDITY_KEY]: true,
-          [constants.FIELD_VALIDITY_MESSAGE_KEY]: errorKey1,
+          [constants.FIELD_VALIDITY_MESSAGE_KEY]: errorMessage1,
         }),
         [fieldName2]: Map({
-          [constants.FIELD_RENDER_KEY]: categoryName + fieldName2,
-          [constants.FIELD_VALUE_KEY]: "",
-          [constants.FIELD_VALIDITY_KEY]: false,
-          [constants.FIELD_VALIDITY_MESSAGE_KEY]: errorKey2,
+          [constants.FIELD_VALIDITY_KEY]: true,
+          [constants.FIELD_VALIDITY_MESSAGE_KEY]: errorMessage2,
         }),
         [fieldName3]: Map({
-          [constants.FIELD_RENDER_KEY]: categoryName + fieldName3,
-          [constants.FIELD_VALUE_KEY]: "",
           [constants.FIELD_VALIDITY_KEY]: false,
-          [constants.FIELD_VALIDITY_MESSAGE_KEY]: errorKey3,
+          [constants.FIELD_VALIDITY_MESSAGE_KEY]: errorMessage3,
         }),
       }),
-      updatingField: null,
-      isFormSubmitting: false,
-      formValidationErrors: new Set(),
-      showValidityStatus: false,
-      isMapPositioned: false,
     });
 
     wrapper.instance().onSubmit(eventStub);
     expect(wrapper.state("formValidationErrors")).toEqual(
-      new Set([errorKey2, errorKey3]),
+      new Set([errorMessage3]),
     );
+  });
+
+  test("multi stage forms render fields correctly", () => {
+    const categoryName = "someMultiStageCategory";
+    const fieldName1 = "test1";
+    const fieldName2 = "test2";
+    const fieldName3 = "test3";
+    const fieldName4 = "test4";
+    const props = {
+      ...defaultProps,
+      selectedCategory: categoryName,
+    };
+
+    const wrapper = shallow(<InputForm {...props} />);
+    wrapper.setState({
+      fields: OrderedMap({
+        [fieldName1]: Map({
+          [constants.FIELD_VALUE_KEY]: "",
+          [constants.FIELD_VALIDITY_KEY]: true,
+        }),
+        [fieldName2]: Map({
+          [constants.FIELD_VALUE_KEY]: "",
+          [constants.FIELD_VALIDITY_KEY]: true,
+        }),
+        [fieldName3]: Map({
+          [constants.FIELD_VALUE_KEY]: "",
+          [constants.FIELD_VALIDITY_KEY]: true,
+        }),
+        [fieldName4]: Map({
+          [constants.FIELD_VALUE_KEY]: "",
+          [constants.FIELD_VALIDITY_KEY]: true,
+        }),
+      }),
+    });
+
+    expect(wrapper.find(FormField)).toHaveLength(3);
+    wrapper.setState({
+      currentStage: 2,
+    });
+    expect(wrapper.find(FormField)).toHaveLength(1);
   });
 });
