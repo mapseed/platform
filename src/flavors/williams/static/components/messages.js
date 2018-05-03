9172942575ef9863b3c4dd1150aee8972c6baa91
Trevor Croxson <goldpbear@users.noreply.github.com>
2018-05-03 11:06:31 -0700

    fix: update VV flavor AppView overrides (#931)

--- src/flavors/williams/static/components/messages.js
+++
@@ -1,8 +0,0 @@
-export default {
-  categorySummaryLabel: "Summary",
-  inputListHeader:
-    "What concerns do you have about this garden or its future development?",
-  subcategorySummaryLabel: "All",
-  recommendationsLabel: "Recommendations",
-  concernsLabel: "Concerns",
-};
