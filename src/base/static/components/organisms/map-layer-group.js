eb7912210b5ab4832f1e56865eb76be8ea5e6b26
goldpbear <tcroxson@gmail.com>
2018-06-19 16:26:04 -0500

    refactor: add MapLayerGroup component

---
+++ src/base/static/components/organisms/map-layer-group.js
@@ -0,0 +1,131 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classNames from "classnames";
+
+import { HorizontalRule } from "../atoms/misc";
+import { Header5 } from "../atoms/typography";
+
+import { mapConfigSelector } from "../../state/ducks/map-config";
+import {
+  setLayerStatus,
+  setBasemap,
+  mapLayersStatusSelector,
+  mapBasemapSelector,
+} from "../../state/ducks/map";
+import MapLayerSelector from "../molecules/map-layer-selector";
+
+const MapLayerGroup = props => {
+  return (
+    <div className={classNames(props.classes, "map-layer-group")}>
+      <HorizontalRule />
+      <Header5>{props.title}</Header5>
+      {props.layers.map(layer => {
+        const isBasemap = !!props.mapConfig.layers.find(
+          layerConfig => layerConfig.id === layer.id,
+        ).isBasemap;
+        const layerStatus = props.layersStatus[layer.id];
+
+        return (
+          <MapLayerSelector
+            key={layer.id}
+            layerId={layer.id}
+            title={layer.title}
+            layerStatus={props.layersStatus[layer.id]}
+            selected={!!(layerStatus && layerStatus.isVisible)}
+            onToggleLayer={layerId => {
+              if (isBasemap && layerStatus && layerStatus.isVisible) {
+                // If the user clicked on the basemap that is already
+                // visible, do nothing.
+                return;
+              } else if (isBasemap) {
+                // If the user clicked on a basemap and there is already a
+                // visible basemap, turn the old basemap off.
+                props.visibleBasemapId &&
+                  props.setLayerStatus(props.visibleBasemapId, {
+                    isVisible: false,
+                  });
+                // Switch the new basemap on.
+                props.setBasemap(layerId);
+                props.setLayerStatus(layerId, {
+                  status: "loading",
+                  isVisible: true,
+                  isBasemap: true,
+                });
+              } else {
+                // Otherwise, toggle the selected layer.
+                props.setLayerStatus(layerId, {
+                  status: "loading",
+                  isVisible:
+                    layerStatus === undefined ? true : !layerStatus.isVisible,
+                });
+              }
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+MapLayerGroup.propTypes = {
+  classes: PropTypes.string,
+  mapConfig: PropTypes.shape({
+    layers: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        url: PropTypes.string,
+        source: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+        slug: PropTypes.string,
+        rules: PropTypes.arrayOf(
+          PropTypes.shape({
+            filter: PropTypes.array,
+            "symbol-layout": PropTypes.object,
+            "symbol-paint": PropTypes.object,
+            "line-layout": PropTypes.object,
+            "line-paint": PropTypes.object,
+            "fill-layout": PropTypes.object,
+            "fill-paint": PropTypes.object,
+          }),
+        ),
+      }),
+    ),
+  }),
+  grouping: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string,
+    }),
+  ),
+  layers: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      visible_default: PropTypes.bool,
+    }),
+  ),
+  layersStatus: PropTypes.shape({
+    isBasemap: PropTypes.bool,
+    isVisible: PropTypes.bool,
+    status: PropTypes.string,
+  }),
+  title: PropTypes.string,
+};
+
+const mapStateToProps = state => ({
+  mapConfig: mapConfigSelector(state),
+  layersStatus: mapLayersStatusSelector(state),
+  visibleBasemapId: mapBasemapSelector(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  setLayerStatus: (layerId, layerStatus) =>
+    dispatch(setLayerStatus(layerId, layerStatus)),
+  setBasemap: layerId => dispatch(setBasemap(layerId)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(MapLayerGroup);
--- src/base/static/components/organisms/map-layer-panel.js
+++ src/base/static/components/organisms/map-layer-panel.js
@@ -3,16 +3,8 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import { leftSidebarPanelConfigSelector } from "../../state/ducks/left-sidebar-config";
-import { mapConfigSelector } from "../../state/ducks/map-config";
-import {
-  setLayerStatus,
-  setBasemap,
-  mapLayersStatusSelector,
-  mapBasemapSelector,
-} from "../../state/ducks/map";
-import { Header4, Header5 } from "../atoms/typography";
-import { HorizontalRule } from "../atoms/misc";
-import MapLayerSelector from "../molecules/map-layer-selector";
+import { Header4 } from "../atoms/typography";
+import MapLayerGroup from "./map-layer-group";
 
 import "./map-layer-panel.scss";
 
@@ -22,117 +14,23 @@ const MapLayerPanel = props => {
       <Header4>{props.mapLayerPanelConfig.title}</Header4>
       {props.mapLayerPanelConfig.groupings &&
         props.mapLayerPanelConfig.groupings.map(grouping => (
-          <div key={grouping.id}>
-            <HorizontalRule />
-            <div className="map-layer-panel__layer-group">
-              <Header5>{grouping.title}</Header5>
-              {grouping.layers.map(layer => {
-                const isBasemap = !!props.mapConfig.layers.find(
-                  layerConfig => layerConfig.id === layer.id,
-                ).isBasemap;
-                return (
-                  <MapLayerSelector
-                    key={layer.id}
-                    layerId={layer.id}
-                    title={layer.title}
-                    layerStatus={props.layersStatus[layer.id]}
-                    selected={
-                      !!(
-                        props.layersStatus[layer.id] &&
-                        props.layersStatus[layer.id].isVisible
-                      )
-                    }
-                    onToggleLayer={layerId => {
-                      if (isBasemap) {
-                        if (
-                          props.layersStatus[layerId] &&
-                          props.layersStatus[layerId].isVisible
-                        ) {
-                          // If the user clicked on the basemap that is already
-                          // visible, do nothing.
-                          return;
-                        }
-                        if (props.visibleBasemapId) {
-                          // Switch the previous basemap off.
-                          props.setLayerStatus(props.visibleBasemapId, {
-                            isVisible: false,
-                          });
-                        }
-                        props.setBasemap(layerId);
-                        // Switch the new basemap on.
-                        props.setLayerStatus(layerId, {
-                          status: "loading",
-                          isVisible: true,
-                          isBasemap: true,
-                        });
-                      } else {
-                        props.setLayerStatus(layerId, {
-                          status: "loading",
-                          isVisible:
-                            props.layersStatus[layer.id] === undefined
-                              ? true
-                              : !props.layersStatus[layerId].isVisible,
-                        });
-                      }
-                    }}
-                  />
-                );
-              })}
-            </div>
-          </div>
+          <MapLayerGroup
+            key={grouping.id}
+            classes="map-layer-panel__layer-group"
+            layers={grouping.layers}
+            title={grouping.title}
+          />
         ))}
     </div>
   );
 };
 
 MapLayerPanel.propTypes = {
-  mapConfig: PropTypes.shape({
-    geolocation_enabled: PropTypes.bool.isRequired,
-    layers: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-        url: PropTypes.string,
-        source: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-        slug: PropTypes.string,
-        rules: PropTypes.arrayOf(
-          PropTypes.shape({
-            filter: PropTypes.array,
-            "symbol-layout": PropTypes.object,
-            "symbol-paint": PropTypes.object,
-            "line-layout": PropTypes.object,
-            "line-paint": PropTypes.object,
-            "fill-layout": PropTypes.object,
-            "fill-paint": PropTypes.object,
-          }),
-        ),
-      }),
-    ),
-    options: PropTypes.shape({
-      map: PropTypes.shape({
-        center: PropTypes.shape({
-          lat: PropTypes.number.isRequired,
-          lng: PropTypes.number.isRequired,
-        }).isRequired,
-        zoom: PropTypes.number.isRequired,
-        minZoom: PropTypes.number.isRequired,
-        maxZoom: PropTypes.number.isRequired,
-      }),
-    }),
-    provider: PropTypes.string,
-  }),
   mapLayerPanelConfig: PropTypes.shape({
     id: PropTypes.string.isRequired,
     icon: PropTypes.string,
     component: PropTypes.string.isRequired,
     title: PropTypes.string,
-    basemaps: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        visible_default: PropTypes.bool,
-      }),
-    ),
     groupings: PropTypes.arrayOf(
       PropTypes.shape({
         id: PropTypes.string.isRequired,
@@ -156,18 +54,6 @@ MapLayerPanel.defaultProps = {
 
 const mapStateToProps = state => ({
   mapLayerPanelConfig: leftSidebarPanelConfigSelector(state, "MapLayerPanel"),
-  mapConfig: mapConfigSelector(state),
-  layersStatus: mapLayersStatusSelector(state),
-  visibleBasemapId: mapBasemapSelector(state),
 });
 
-const mapDispatchToProps = dispatch => ({
-  setLayerStatus: (layerId, layerStatus) =>
-    dispatch(setLayerStatus(layerId, layerStatus)),
-  setBasemap: layerId => dispatch(setBasemap(layerId)),
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(MapLayerPanel);
+export default connect(mapStateToProps)(MapLayerPanel);
