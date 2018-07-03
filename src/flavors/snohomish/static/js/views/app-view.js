f5dab90ab9565acb7acef69f8f1f5087f9995766
goldpbear <tcroxson@gmail.com>
2018-07-03 09:37:35 -0700

    feat(snohomish): add layer panel header toggle

--- src/flavors/snohomish/static/js/views/app-view.js
+++ src/flavors/snohomish/static/js/views/app-view.js
@@ -1,7 +1,26 @@
 const AppView = require("../../../../../base/static/js/views/app-view.js");
 const Util = require("../../../../../base/static/js/utils.js");
+// BEGIN CUSTOM CODE
+import emitter from "../../../../../base/static/utils/emitter";
+// END CUSTOM CODE
 
 module.exports = AppView.extend({
+  events: {
+    "click #add-place": "onClickAddPlaceBtn",
+    "click .close-btn": "onClickClosePanelBtn",
+    "click .right-sidebar__collapse-btn": "onToggleSidebarVisibility",
+    "click .list-toggle-btn": "toggleListView",
+    // BEGIN CUSTOM CODE
+    "click .show-layer-panel": "showLayerPanel",
+    // END CUSTOM CODE
+  },
+
+  // BEGIN CUSTOM CODE
+  showLayerPanel: () => {
+    emitter.emit("nav-layer-btn:toggle");
+  },
+  // END CUSTOM CODE
+
   viewPage: function(slug) {
     var pageConfig = Util.findPageConfig(this.options.pagesConfig, {
         slug: slug,
