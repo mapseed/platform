b03cb80aec5227b7213fca437bf8124657d9c969
goldpbear <tcroxson@gmail.com>
2018-08-12 13:11:54 -0700

    feat(datepicker): add ongoing option

--- src/base/static/components/form-fields/types/datetime-field.js
+++ src/base/static/components/form-fields/types/datetime-field.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Datetime from "react-datetime";
 import moment from "moment";
+import classNames from "classnames";
 
 import "react-datetime/css/react-datetime.css";
 
 import constants from "../../../constants";
 
 import "./datetime-field.scss";
+
+const ONGOING_VALUE = "ONGOING";
 
 class DatetimeField extends Component {
   componentDidMount() {
@@ -22,19 +25,46 @@ class DatetimeField extends Component {
         timeFormat={this.props.timeFormat}
         renderInput={(datetimeProps, openCalendar) => {
           return (
-            <input
-              type="text"
-              ref={input => {
-                this.inputRef = input;
-              }}
-              className="datetime-field__input"
-              onFocus={openCalendar}
-              placeholder={this.props.placeholder || "Select a date"}
-              value={
-                this.props.value &&
-                moment(this.props.value).format(this.props.displayFormat)
-              }
-            />
+            <div className="datetime-field__input-wrapper">
+              {this.props.ongoingLabel && (
+                <label
+                  className={classNames("datetime-field__label-ongoing", {
+                    "datetime-field__label-ongoing--toggled":
+                      this.props.value === ONGOING_VALUE,
+                  })}
+                >
+                  <input
+                    className="datetime-field__input-ongoing"
+                    type="checkbox"
+                    checked={this.props.value === ONGOING_VALUE}
+                    onChange={e =>
+                      this.props.onChange(
+                        this.props.name,
+                        e.target.checked ? ONGOING_VALUE : "",
+                      )
+                    }
+                  />
+                  <em>{this.props.ongoingLabel}</em>
+                </label>
+              )}
+              <input
+                type="text"
+                ref={input => {
+                  this.inputRef = input;
+                }}
+                className="datetime-field__input"
+                onFocus={openCalendar}
+                placeholder={this.props.placeholder || "Select a date"}
+                value={
+                  (this.props.value &&
+                    this.props.value !== ONGOING_VALUE &&
+                    moment(this.props.value).format(
+                      this.props.displayFormat,
+                    )) ||
+                  ""
+                }
+              />
+            </div>
           );
         }}
         closeOnSelect={true}
@@ -59,6 +89,7 @@ DatetimeField.propTypes = {
   isAutoFocusing: PropTypes.bool.isRequired,
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
+  ongoingLabel: PropTypes.string,
   placeholder: PropTypes.string,
   timeFormat: PropTypes.oneOfType([PropTypes.string, PropTypes.bool])
     .isRequired,
