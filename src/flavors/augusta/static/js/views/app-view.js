ac94a10bc5482dc53b26a68dbf9108de114efe9e
jalmogo <you@example.com>
2018-08-22 01:25:49 -0700

    feat(theming): update vv flavors with new css-in-js theming

--- src/flavors/augusta/static/js/views/app-view.js
+++ src/flavors/augusta/static/js/views/app-view.js
@@ -10,6 +10,8 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "../../../../../base/static/state/reducers";
 import mapseedApiClient from "../../../../../base/static/client/mapseed-api-client";
+import { ThemeProvider } from "emotion-theming";
+import theme from "../../../../../theme";
 // TODO(luke): This should be the only instance of our config singleton.
 // Eventually, it will be removed once we start fetching the config
 // from the api:
@@ -50,6 +52,10 @@ module.exports = AppView.extend({
     // component:
     store.dispatch(setConfig(config));
     const storeState = store.getState();
+    const flavorTheme = storeState.config.app.theme;
+    const adjustedTheme = flavorTheme
+      ? ancestorTheme => ({ ...ancestorTheme, ...flavorTheme })
+      : {};
 
     languageModule.changeLanguage(this.options.languageCode);
 
@@ -165,12 +171,16 @@ module.exports = AppView.extend({
     }).render();
 
     ReactDOM.render(
-      <UserMenu
-        router={this.options.router}
-        apiRoot={storeState.config.app.api_root}
-        currentUser={Shareabouts.bootstrapped.currentUser}
-        datasetDownloadConfig={storeState.config.app.dataset_download}
-      />,
+      <ThemeProvider theme={theme}>
+        <ThemeProvider theme={adjustedTheme}>
+          <UserMenu
+            router={this.options.router}
+            apiRoot={storeState.config.app.api_root}
+            currentUser={Shareabouts.bootstrapped.currentUser}
+            datasetDownloadConfig={storeState.config.app.dataset_download}
+          />
+        </ThemeProvider>
+      </ThemeProvider>,
       document.getElementById("auth-nav-container"),
     );
 
@@ -248,9 +258,13 @@ module.exports = AppView.extend({
     // REACT PORT SECTION /////////////////////////////////////////////////////
     if (this.options.mapConfig.geocoding_bar_enabled) {
       ReactDOM.render(
-        <Provider store={store}>
-          <GeocodeAddressBar mapConfig={this.options.mapConfig} />
-        </Provider>,
+        <ThemeProvider theme={theme}>
+          <ThemeProvider theme={adjustedTheme}>
+            <Provider store={store}>
+              <GeocodeAddressBar mapConfig={this.options.mapConfig} />
+            </Provider>
+          </ThemeProvider>
+        </ThemeProvider>,
         document.getElementById("geocode-address-bar"),
       );
     }
@@ -281,13 +295,17 @@ module.exports = AppView.extend({
       );
 
       ReactDOM.render(
-        <Provider store={store}>
-          <InfoModal
-            parentId="info-modal-container"
-            isModalOpen={true}
-            {...modalContent}
-          />
-        </Provider>,
+        <ThemeProvider theme={theme}>
+          <ThemeProvider theme={adjustedTheme}>
+            <Provider store={store}>
+              <InfoModal
+                parentId="info-modal-container"
+                isModalOpen={true}
+                {...modalContent}
+              />
+            </Provider>
+          </ThemeProvider>
+        </ThemeProvider>,
         document.getElementById("info-modal-container"),
       );
     });
@@ -428,16 +446,25 @@ module.exports = AppView.extend({
     $(".show-the-list").addClass("is-visuallyhidden");
     $(".show-the-map").removeClass("is-visuallyhidden");
     $("#list-container").addClass("is-exposed");
+    const storeState = store.getState();
+    const flavorTheme = storeState.config.app.theme;
+    const adjustedTheme = flavorTheme
+      ? ancestorTheme => ({ ...ancestorTheme, ...flavorTheme })
+      : {};
 
     // NOTE: we hard-code the augusta-input collection here
     ReactDOM.render(
-      <Provider store={store}>
-        <InputExplorer
-          appConfig={this.options.appConfig}
-          placeConfig={this.options.placeConfig.place_detail}
-          communityInput={this.places["augusta-input"]}
-        />
-      </Provider>,
+      <ThemeProvider theme={theme}>
+        <ThemeProvider theme={adjustedTheme}>
+          <Provider store={store}>
+            <InputExplorer
+              appConfig={this.options.appConfig}
+              placeConfig={this.options.placeConfig.place_detail}
+              communityInput={this.places["augusta-input"]}
+            />
+          </Provider>
+        </ThemeProvider>
+      </ThemeProvider>,
       document.querySelector("#list-container"),
     );
   },
