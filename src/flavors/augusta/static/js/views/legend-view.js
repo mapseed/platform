311ae98fd31fb31a79b54127ace275a6aa08e90e
Trevor Croxson <goldpbear@users.noreply.github.com>
2018-09-30 17:47:42 -0700

    refactor: remove unused files (#1020)

--- src/flavors/augusta/static/js/views/legend-view.js
+++
@@ -1,8 +0,0 @@
-const LegendView = require("../../../../../base/static/js/views/legend-view.js");
-
-module.exports = LegendView.extend({
-  render: function() {
-    this.$el.html(Handlebars.templates["legend"]());
-    return this;
-  },
-});
