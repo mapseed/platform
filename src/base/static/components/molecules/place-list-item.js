faa8fd3fb7b61c0521d9ea6dba5a4d3b9d8784f2
jalmogo <you@example.com>
2018-10-26 15:44:40 -0700

    feat(PlaceListItem): now using IconButton

--- src/base/static/components/molecules/place-list-item.js
+++ src/base/static/components/molecules/place-list-item.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "react-emotion";
 import PropTypes from "prop-types";
-import { Button } from "../atoms/buttons";
+import { Button, IconButton } from "../atoms/buttons";
 import { Header3 } from "../atoms/typography";
 import { UserAvatar } from "../atoms/imagery";
 import { Paragraph, SmallText } from "../atoms/typography";
@@ -9,12 +9,15 @@ import { placeConfigSelector } from "../../state/ducks/place-config";
 import { connect } from "react-redux";
 import { translate } from "react-i18next";
 import { HorizontalRule } from "../atoms/layout";
+import { Link } from "../atoms/typography";
+import TwitterIcon from "../atoms/icons/Twitter";
+import FacebookIcon from "../atoms/icons/Facebook";
 
 const PlaceContainer = styled("div")({
   display: "flex",
   flexDirection: "column",
   overflow: "hidden",
-  margin: "16px",
+  margin: "0px 16px 16px 16px",
 });
 
 const Header = styled("div")({
@@ -60,7 +63,13 @@ const PlaceSocialContainer = styled("div")({
   display: "flex",
   flex: "0 40%",
   maxWidth: "160px",
-  justifyContent: "space-around",
+  justifyContent: "flex-end",
+  alignItems: "center",
+});
+
+const SocialMediaButton = styled(IconButton)({
+  flex: "0 1",
+  marginLeft: "16px",
 });
 
 const PlaceListItem = props => {
@@ -80,8 +89,8 @@ const PlaceListItem = props => {
             <Header3>{props.place.title}</Header3>
           </PlaceTitle>
           <PlaceSocialContainer>
-            <div style={{ flex: "0 40%" }}>{`facebook`}</div>
-            <div style={{ flex: "0 40%" }}>{`twitter`}</div>
+            <SocialMediaButton icon="facebook" size="small" />
+            <SocialMediaButton icon="twitter" size="small" />
           </PlaceSocialContainer>
         </Header>
         <Body>
