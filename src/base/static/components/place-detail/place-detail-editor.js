b7b97eaf87dbc4b1473feb0cbf696b4ea46e3284
goldpbear <tcroxson@gmail.com>
2019-03-05 15:52:35 -0800

    chore: tests

--- src/base/static/components/place-detail/place-detail-editor.js
+++ src/base/static/components/place-detail/place-detail-editor.js
@@ -64,7 +64,7 @@ class PlaceDetailEditor extends Component {
             .set("value", this.props.place[field.name])
             .set("config", fieldConfig)
             .set(
-              // A field will be hidden if it is explicitly declared as 
+              // A field will be hidden if it is explicitly declared as
               // hidden_default in the config, or if it is restricted to a
               // group and the current user is not in that group or is not in
               // the administrators group.
