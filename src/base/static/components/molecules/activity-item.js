c908bc112bfc13f714e2c6c229c017940a333955
jalmogo <you@example.com>
2019-03-27 22:27:39 -0700

    fix(ActivityItem): aria li / ul fixes

--- src/base/static/components/molecules/activity-item.js
+++ src/base/static/components/molecules/activity-item.js
@@ -1,35 +1,11 @@
-import React from "react";
+/** @jsx jsx */
+import * as React from "react";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
+import { jsx } from "@emotion/core";
 
 import { UserAvatar } from "../atoms/imagery";
 import { RegularText, Link } from "../atoms/typography";
-
-const ActivityItemContainer = styled("div")({
-  position: "relative",
-  listStyle: "none",
-  borderBottom: "1px solid #888",
-});
-
-const ActivityLink = styled(props => {
-  return (
-    <Link
-      className={props.className}
-      href={props.href}
-      rel="internal"
-      router={props.router}
-    >
-      {props.children}
-    </Link>
-  );
-})(props => ({
-  display: "block",
-  textTransform: "none",
-  "&:hover": {
-    backgroundColor: props.theme.brand.accent,
-    color: "#fff",
-  },
-}));
 
 const UserAvatarContainer = styled("div")({
   position: "absolute",
@@ -42,25 +18,40 @@ const ActionTextContainer = styled("div")({
 });
 
 const ActivityItem = props => (
-  <ActivityItemContainer>
-    <ActivityLink href={props.url} router={props.router}>
-      <li className={props.className}>
-        <UserAvatarContainer>
-          <UserAvatar />
-        </UserAvatarContainer>
-        <ActionTextContainer>
-          <RegularText weight="black">{props.submitterName} </RegularText>
-          <RegularText> {props.actionText}: </RegularText>
-          <RegularText>{props.title}</RegularText>
-        </ActionTextContainer>
-      </li>
-    </ActivityLink>
-  </ActivityItemContainer>
+  <li
+    css={{
+      position: "relative",
+      listStyle: "none",
+      borderBottom: "1px solid #888",
+    }}
+  >
+    <Link
+      css={theme => ({
+        display: "block",
+        textTransform: "none",
+        "&:hover": {
+          backgroundColor: theme.brand.accent,
+          color: "#fff",
+        },
+      })}
+      href={props.url}
+      rel="internal"
+      router={props.router}
+    >
+      <UserAvatarContainer>
+        <UserAvatar />
+      </UserAvatarContainer>
+      <ActionTextContainer>
+        <RegularText weight="black">{props.submitterName} </RegularText>
+        <RegularText> {props.actionText}: </RegularText>
+        <RegularText>{props.title}</RegularText>
+      </ActionTextContainer>
+    </Link>
+  </li>
 );
 
 ActivityItem.propTypes = {
   actionText: PropTypes.string.isRequired,
-  className: PropTypes.string,
   router: PropTypes.instanceOf(Backbone.Router).isRequired,
   submitterName: PropTypes.string.isRequired,
   title: PropTypes.string,
