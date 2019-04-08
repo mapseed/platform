7ca65009a91eb9f13b671f32c548ea1f74fa0273
goldpbear <tcroxson@gmail.com>
2019-04-08 10:23:36 -0700

    refactor(inviteModal)

--- src/base/static/components/organisms/invite-modal.js
+++ src/base/static/components/organisms/invite-modal.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-
+import { withRouter } from "react-router-dom"; 
 import { RegularText } from "../atoms/typography";
 import { CloseButton } from "../atoms/buttons";
 import { Button } from "../atoms/buttons";
