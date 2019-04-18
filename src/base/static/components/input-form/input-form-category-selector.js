519e771c543ac77555dd27dc562704fd21aa32de
goldpbear <tcroxson@gmail.com>
2019-04-18 10:44:42 -0700

    feat(InputFormCategorySelector): header info on category page

--- src/base/static/components/input-form/input-form-category-selector.js
+++ src/base/static/components/input-form/input-form-category-selector.js
@@ -1,11 +1,47 @@
+/** @jsx jsx */
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { css, jsx } from "@emotion/core";
+import { withTheme } from "emotion-theming";
 
 import InputFormCategoryButton from "./input-form-category-button";
+import { SmallTitle, RegularText } from "../atoms/typography";
+import { HorizontalRule } from "../atoms/layout";
 
 import { hasGroupAbilitiesInDatasets } from "../../state/ducks/user";
 import { hasAnonAbilitiesInDataset } from "../../state/ducks/datasets-config";
+import {
+  placeConfigSelector,
+  placeConfigPropType,
+} from "../../state/ducks/place-config";
+
+const CategoryHeader = withTheme(props => (
+  <div
+    css={css`
+      margin-bottom: 16px;
+    `}
+  >
+    <HorizontalRule spacing="small" />
+    <SmallTitle
+      css={css`
+        margin-left: 16px;
+        color: #555;
+      `}
+    >
+      {props.header}
+    </SmallTitle>
+    <RegularText
+      css={css`
+        margin-left: 16px;
+        color: #555;
+        font-style: italic;
+      `}
+    >
+      {props.description}
+    </RegularText>
+  </div>
+));
 
 class InputFormCategorySelector extends Component {
   state = {
@@ -20,10 +56,21 @@ class InputFormCategorySelector extends Component {
   }
 
   render() {
+    const datasetSlugs = new Set();
+
     return (
       <>
         {this.props.visibleCategoryConfigs.map(config => {
           const isSelected = this.props.selectedCategory === config.category;
+
+          let isWithFormCategoryHeader = false;
+          if (
+            !datasetSlugs.has(config.datasetSlug) &&
+            this.props.placeConfig.formCategoryHeaders[config.datasetSlug]
+          ) {
+            isWithFormCategoryHeader = true;
+            datasetSlugs.add(config.datasetSlug);
+          }
 
           // NOTE: These permissions checks do not allow for the possibility
           // of controlling category visibility on a group-by-group basis,
@@ -39,19 +86,36 @@ class InputFormCategorySelector extends Component {
                 submissionSet: "places",
                 datasetSlugs: [config.datasetSlug],
               })) && (
-              <InputFormCategoryButton
-                isSelected={isSelected}
-                isCategoryMenuCollapsed={this.state.isCollapsed}
-                isSingleCategory={
-                  this.props.visibleCategoryConfigs.length === 1
-                }
-                key={config.category}
-                categoryName={config.category}
-                onCategoryChange={evt => {
-                  this.setCategory(evt.target.value, false);
-                }}
-                onExpandCategories={() => this.setState({ isCollapsed: false })}
-              />
+              <div key={config.category}>
+                {isWithFormCategoryHeader && (
+                  <CategoryHeader
+                    header={
+                      this.props.placeConfig.formCategoryHeaders[
+                        config.datasetSlug
+                      ].header
+                    }
+                    description={
+                      this.props.placeConfig.formCategoryHeaders[
+                        config.datasetSlug
+                      ].description
+                    }
+                  />
+                )}
+                <InputFormCategoryButton
+                  isSelected={isSelected}
+                  isCategoryMenuCollapsed={this.state.isCollapsed}
+                  isSingleCategory={
+                    this.props.visibleCategoryConfigs.length === 1
+                  }
+                  categoryName={config.category}
+                  onCategoryChange={evt => {
+                    this.setCategory(evt.target.value, false);
+                  }}
+                  onExpandCategories={() =>
+                    this.setState({ isCollapsed: false })
+                  }
+                />
+              </div>
             )
           );
         })}
@@ -64,6 +128,7 @@ InputFormCategorySelector.propTypes = {
   hasAnonAbilitiesInDataset: PropTypes.func.isRequired,
   hasGroupAbilitiesInDatasets: PropTypes.func.isRequired,
   onCategoryChange: PropTypes.func.isRequired,
+  placeConfig: placeConfigPropType.isRequired,
   selectedCategory: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   visibleCategoryConfigs: PropTypes.array.isRequired,
 };
@@ -78,6 +143,7 @@ const mapStateToProps = state => ({
     }),
   hasAnonAbilitiesInDataset: ({ abilities, submissionSet, datasetSlug }) =>
     hasAnonAbilitiesInDataset({ state, abilities, submissionSet, datasetSlug }),
+  placeConfig: placeConfigSelector(state),
 });
 
 export default connect(mapStateToProps)(InputFormCategorySelector);
