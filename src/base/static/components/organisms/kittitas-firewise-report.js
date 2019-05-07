a8d6f8f46198186e8849ce32225f048d31c501d4
goldpbear <tcroxson@gmail.com>
2019-05-07 15:59:44 -0700

    feat(kittitasPrintReport): buildout

--- src/base/static/components/organisms/kittitas-firewise-report.js
+++ src/base/static/components/organisms/kittitas-firewise-report.js
@@ -4,11 +4,13 @@ import { css, jsx } from "@emotion/core";
 import styled from "@emotion/styled";
 
 import {
+  RegularText,
   LargeText,
   SmallTitle,
   LargeTitle,
   ExternalLink,
 } from "../atoms/typography";
+import { HorizontalRule } from "../atoms/layout";
 import { Image, FontAwesomeIcon } from "../atoms/imagery";
 import fieldResponseFilter from "../../utils/field-response-filter";
 import { placePropType } from "../../state/ducks/places";
@@ -25,7 +27,7 @@ const RightSidebar = styled("section")({
 
 const SectionTitle = styled(LargeTitle)({
   textTransform: "uppercase",
-  marginTop: 0,
+  margin: 0,
   fontFamily: "PTSansBold,sans-serif",
 });
 
@@ -68,9 +70,61 @@ const ResourceRelatedInfoItem = styled(props => {
       <LargeText>{props.children}</LargeText>
     </li>
   );
-})(props => ({
+})(() => ({
   display: "flex",
   alignItems: "center",
+  marginTop: "16px",
+}));
+
+const MainPanel = styled("section")({
+  width: "640px",
+  height: "100%",
+});
+
+const MainPanelSection = styled("div")({
+  marginBottom: "36px",
+});
+
+const MainPanelTitle = styled(props => {
+  return (
+    <div className={props.className}>
+      <div
+        css={css`
+          display: flex;
+          align-items: baseline;
+        `}
+      >
+        <Image
+          css={css`
+            align-self: baseline;
+            width: 48px;
+          `}
+          src="/static/css/images/fire-icon.png"
+          alt="Fire icon"
+        />
+        <SectionTitle>{props.children}</SectionTitle>
+      </div>
+      <HorizontalRule spacing="tiny" />
+    </div>
+  );
+})(() => ({
+  borderLeft: "6px solid #e49494",
+  padding: "8px 0 8px 16px",
+  marginBottom: "8px",
+}));
+
+const MainPanelSectionInfo = styled("div")({
+  paddingLeft: "36px",
+});
+
+const MainPanelSectionText = styled(props => {
+  return (
+    <RegularText className={props.className} display="block">
+      {props.children}
+    </RegularText>
+  );
+})(() => ({
+  margin: "0 0 16px 0",
 }));
 
 const KittitasFirewiseReport = props => {
@@ -84,7 +138,6 @@ const KittitasFirewiseReport = props => {
         margin: 0 auto;
         padding: 24px;
         color: #444;
-        background-color: #eee;
       `}
     >
       <header
@@ -118,7 +171,7 @@ const KittitasFirewiseReport = props => {
         css={css`
           position: relative;
           height: 1319px;
-          padding: 20px;
+          padding: 20px 20px 20px 0;
         `}
       >
         <RightSidebar>
@@ -138,20 +191,104 @@ const KittitasFirewiseReport = props => {
             <ResourceRelatedInfo>
               <ResourceRelatedInfoItem faClassname="fas fa-globe">
                 <ExternalLinkWithBreak href="https://www.facebook.com/KittitasFACC">
-                  https://www.facebook.com/KittitasFACC
+                  www.facebook.com/KittitasFACC
+                </ExternalLinkWithBreak>
+              </ResourceRelatedInfoItem>
+              <ResourceRelatedInfoItem faClassname="fas fa-globe">
+                <ExternalLinkWithBreak href="https://fireadaptedwashington.org">
+                  fireadaptedwashington.org
+                </ExternalLinkWithBreak>
+              </ResourceRelatedInfoItem>
+              <ResourceRelatedInfoItem faClassname="fas fa-info-circle">
+                <LargeText>
+                  Connect with your Fire Adapted Community to learn how to live
+                  proactively with wildfire
+                </LargeText>
+              </ResourceRelatedInfoItem>
+            </ResourceRelatedInfo>
+          </ResourcesInfo>
+          <ResourcesInfo>
+            <SectionSubtitle>Free Onsite Risk Auditing</SectionSubtitle>
+            <ResourceRelatedInfo>
+              <ResourceRelatedInfoItem faClassname="fas fa-phone">
+                [509-925-3352 x202]
+              </ResourceRelatedInfoItem>
+              <ResourceRelatedInfoItem faClassname="fas fa-info-circle">
+                <LargeText>
+                  Free risk auditing is available through your local
+                  Conservation District
+                </LargeText>
+              </ResourceRelatedInfoItem>
+            </ResourceRelatedInfo>
+          </ResourcesInfo>
+          <ResourcesInfo>
+            <SectionSubtitle>
+              Kittitas County Community Wildfire Protection Plan
+            </SectionSubtitle>
+            <ResourceRelatedInfo>
+              <ResourceRelatedInfoItem faClassname="fas fa-globe">
+                <ExternalLinkWithBreak href="https://bit.ly/fdfj3D23d">
+                  bit.ly/fdfj3D23d
                 </ExternalLinkWithBreak>
               </ResourceRelatedInfoItem>
             </ResourceRelatedInfo>
           </ResourcesInfo>
+          <ResourcesInfo>
+            <SectionSubtitle>Firewise Information</SectionSubtitle>
+            <ResourceRelatedInfo>
+              <ResourceRelatedInfoItem faClassname="fas fa-globe">
+                <ExternalLinkWithBreak href="https://bit.ly/aa42nnd22">
+                  bit.ly/aa42nnd22
+                </ExternalLinkWithBreak>
+              </ResourceRelatedInfoItem>
+            </ResourceRelatedInfo>
+          </ResourcesInfo>
+          <ResourcesInfo>
+            <SectionSubtitle>Maps & Data</SectionSubtitle>
+            <ResourceRelatedInfo>
+              <ResourceRelatedInfoItem faClassname="fas fa-globe">
+                <ExternalLinkWithBreak href="https://kittitasfirewise.mapseed.org">
+                  kittitasfirewise.mapseed.org
+                </ExternalLinkWithBreak>
+              </ResourceRelatedInfoItem>
+              <ResourceRelatedInfoItem faClassname="fas fa-info-circle">
+                <LargeText>
+                  Visit Kittitas County’s Firewise map to explore fire risk and
+                  other critical data
+                </LargeText>
+              </ResourceRelatedInfoItem>
+            </ResourceRelatedInfo>
+          </ResourcesInfo>
         </RightSidebar>
-        <section
-          css={css`
-            padding: 24px 10px 10px 10px;
-            background-color: rgba(229, 207, 207, 0.1);
-          `}
-        >
-          <p>{place.emergency_kit}</p>
-        </section>
+        <MainPanel>
+          <MainPanelSection>
+            <MainPanelTitle>Wildfire In Kittitas County</MainPanelTitle>
+            <MainPanelSectionInfo>
+              <Image
+                css={css`
+                  margin: 0 0 8px 8px;
+                  float: right;
+                  width: 65%;
+                `}
+                src="/static/css/images/taylor-bridge-fire.jpg"
+                alt="Taylor Bridge Fire"
+              />
+              <MainPanelSectionText>
+                Thank you for taking the time to report your wildfire
+                preparedness actions.
+              </MainPanelSectionText>
+              <MainPanelSectionText>
+                This report summarizes the wildfire risk in the vicinity of your
+                property, as reported on the Kittitas County Firewise map.
+              </MainPanelSectionText>
+              <MainPanelSectionText>
+                Even if the fire risk in your area is low, be aware that all
+                locations in Kittitas County experience some level of wildfire
+                risk.
+              </MainPanelSectionText>
+            </MainPanelSectionInfo>
+          </MainPanelSection>
+        </MainPanel>
       </section>
     </div>
   );
