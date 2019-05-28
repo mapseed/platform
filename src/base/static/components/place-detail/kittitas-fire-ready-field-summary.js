eb79d7cf985e0dfb22b2cf79f19f886599269749
goldpbear <tcroxson@gmail.com>
2019-05-27 21:01:33 -0700

    refactor(kittitas): content

--- src/base/static/components/place-detail/kittitas-fire-ready-field-summary.js
+++ src/base/static/components/place-detail/kittitas-fire-ready-field-summary.js
@@ -1,7 +1,9 @@
+/** @jsx jsx */
 import React from "react";
 import PropTypes from "prop-types";
+import { css, jsx } from "@emotion/core";
 
-import { SmallTitle } from "../atoms/typography";
+import { LargeText } from "../atoms/typography";
 import CoverImage from "../molecules/cover-image";
 
 import { placePropType } from "../../state/ducks/places";
@@ -30,21 +32,50 @@ const KittitasFireReadyFieldSummary = props => {
     "create_fuel_breaks",
     "move_flammable_material",
   ].filter(action => props.place[action] === "yes").length;
+  const totalActions = numVegetationActions + numBuildingActions;
 
   return (
-    <div>
+    <>
+      {totalActions > 0 && (
+        <div
+          css={css`
+            margin-top: 16px;
+            margin-left: 32px;
+            margin-bottom: 32px;
+            display: flex;
+            align-items: center;
+          `}
+        >
+          <span
+            css={css`
+              color: #fff;
+              font-family: PTSansBold, sans-serif;
+              font-size: 2rem;
+              padding: 16px;
+              background-color: #e57c03;
+              border-radius: 50%;
+              text-align: center;
+            `}
+          >
+            {totalActions}{" "}
+          </span>
+          <LargeText
+            css={css`
+              display: inline-block;
+              margin-left: 16px;
+              color: #888;
+            `}
+          >
+            wildfire prevention actions
+          </LargeText>
+        </div>
+      )}
       {props.place.attachments
         .filter(attachment => attachment.type === "CO")
         .map((attachment, i) => (
           <CoverImage key={i} imageUrl={attachment.file} />
         ))}
-      {numVegetationActions > 0 && (
-        <SmallTitle>
-          Number of Wildfire Prevention Actions:{" "}
-          {numVegetationActions + numBuildingActions}
-        </SmallTitle>
-      )}
-    </div>
+    </>
   );
 };
 
