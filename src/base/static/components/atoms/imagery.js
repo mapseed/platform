0f31e32807695604bd065615443a0a765804b6d2
goldpbear <tcroxson@gmail.com>
2019-08-06 09:33:31 -0700

    refactor(bellevue-bike-share): content

--- src/base/static/components/atoms/imagery.js
+++ src/base/static/components/atoms/imagery.js
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import React, { useState, useEffect } from "react";
 import { css, jsx } from "@emotion/core";
+import { withTheme } from "emotion-theming";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
 import mq from "../../../../media-queries";
@@ -50,7 +51,7 @@ FontAwesomeIcon.defaultProps = {
   textShadow: "initial",
 };
 
-const ProgressBar = props => {
+const ProgressBar = withTheme(props => {
   return (
     <div
       css={css`
@@ -58,7 +59,7 @@ const ProgressBar = props => {
         align-items: center;
         flex: 1;
         padding: 0 5px 0 5px;
-        background-color: #ccc;
+        background-color: #eaeaea;
         height: 20px;
         border-radius: 10px;
       `}
@@ -73,14 +74,14 @@ const ProgressBar = props => {
       />
     </div>
   );
-};
+});
 
 ProgressBar.propTypes = {
   currentProgress: PropTypes.number.isRequired,
   total: PropTypes.number.isRequired,
 };
 const SiteLogo = styled(props => {
-  return <img src={props.src} alt={props.alt} className={props.className} />;
+  return <img id="mapseed-site-logo" src={props.src} alt={props.alt} className={props.className} />;
 })(() => ({
   [mq[0]]: {
     maxWidth: "250px",
