7471c821d6716944755418bb34e6388e38c4a2bc
jalmogo <you@example.com>
2019-08-08 12:36:24 -0700

    fix(emotion-styled): update ts imports

--- src/base/static/components/organisms/right-sidebar.tsx
+++ src/base/static/components/organisms/right-sidebar.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import * as React from "react";
 import { connect } from "react-redux";
-import styled from "@emotion/styled";
+import styled from "../../utils/styled";
 import { jsx } from "@emotion/core";
 import { ChevronRight, ChevronLeft } from "@material-ui/icons";
 
